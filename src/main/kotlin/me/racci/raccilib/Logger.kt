f34b250d55adf06c37534cc4107c1618524038a8
Racci <racci@sylphmc.com>
2021-09-09 23:21:58 +1000

    fix: Missed files
    woops missed these files in the push

--- src/main/kotlin/me/racci/raccilib/Logger.kt
+++ src/main/kotlin/me/racci/raccilib/Logger.kt
@@ -7,14 +7,14 @@ import org.bukkit.Bukkit
 import org.bukkit.ChatColor
 import java.io.IOException
 
-fun log(level: Level, message: String, ex: Exception? = null) {
+fun log(level: Level, message: String) {
     when(level) {
-        Level.ERROR -> Bukkit.getConsoleSender().sendMessage(colour("&4[&c&lERROR&4] &f$message", true)!! + ex!!)
-        Level.WARNING -> Bukkit.getConsoleSender().sendMessage(colour("&e[&6&lWARNING&e] &f$message", true)!! + ex!!)
-        Level.INFO -> Bukkit.getConsoleSender().sendMessage(colour("&8[&e&lINFO&8] &f$message", true)!! + ex!!)
-        Level.DEBUG -> Bukkit.getConsoleSender().sendMessage(colour("&7[&f&lDEBUG&7] &f$message", true)!! + ex!!)
-        Level.SUCCESS -> Bukkit.getConsoleSender().sendMessage(colour("&2[&a&lSUCCESS&2] &f$message", true)!! + ex!!)
-        Level.OUTLINE -> Bukkit.getConsoleSender().sendMessage(colour("&7$message", true)!! + ex!!)
+        Level.ERROR -> Bukkit.getConsoleSender().sendMessage(colour("&4[&c&lERROR&4] &f$message", true)!!)
+        Level.WARNING -> Bukkit.getConsoleSender().sendMessage(colour("&e[&6&lWARNING&e] &f$message", true)!!)
+        Level.INFO -> Bukkit.getConsoleSender().sendMessage(colour("&8[&e&lINFO&8] &f$message", true)!!)
+        Level.DEBUG -> Bukkit.getConsoleSender().sendMessage(colour("&7[&f&lDEBUG&7] &f$message", true)!!)
+        Level.SUCCESS -> Bukkit.getConsoleSender().sendMessage(colour("&2[&a&lSUCCESS&2] &f$message", true)!!)
+        Level.OUTLINE -> Bukkit.getConsoleSender().sendMessage(colour("&7$message", true)!!)
     }
 }
 

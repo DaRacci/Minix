98d4ab7e379252ef0fae1678877fea03c0ef74cc
Racci <racci@sylphmc.com>
2021-09-10 18:23:55 +1000

    Fixed exception error with checking if a block is liquid

--- src/main/kotlin/me/racci/raccilib/utils/blocks/BlockTypeUtils.kt
+++ src/main/kotlin/me/racci/raccilib/utils/blocks/BlockTypeUtils.kt
@@ -7,9 +7,10 @@ import org.bukkit.block.Block
 import org.bukkit.block.data.Waterlogged
 
 fun isLiquid(block: Block): Int {
+    val blockData = block.blockData
     return when(block.type) {
         Material.WATER -> 1
         Material.LAVA -> 2
-        else -> if((block.blockData as Waterlogged).isWaterlogged) 1 else 0
+        else -> if((blockData is Waterlogged) && blockData.isWaterlogged) 1 else 0
     }
 }

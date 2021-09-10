57803145bf4ee9bbb642acae06f368c631598287
Racci <racci@sylphmc.com>
2021-09-10 17:55:56 +1000

    refactor: Updated some stuff to reduce file size, under 1mb again now instead of 8mb, fixed some errors on startup with kotlin

---
+++ src/main/kotlin/me/racci/raccilib/skedule/Extensions.kt
@@ -0,0 +1,15 @@
+@file:Suppress("unused")
+@file:JvmName("Extensions")
+package me.racci.raccilib.skedule
+
+suspend inline fun <T> BukkitSchedulerController.runWithContext(context: SynchronizationContext, block: BukkitSchedulerController.() -> T): T {
+    val before = currentContext()
+    if (before != context) {
+        switchContext(context)
+    }
+    val r = block()
+    if (before != context) {
+        switchContext(before)
+    }
+    return r
+}

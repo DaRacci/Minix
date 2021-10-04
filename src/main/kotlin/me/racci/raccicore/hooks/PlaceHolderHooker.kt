59f3059dd7519b7877940e34ca353e42aa98a150
Racci <racci@sylphmc.com>
2021-10-05 00:42:23 +1100

    Quick push

---
+++ src/main/kotlin/me/racci/raccicore/hooks/PlaceHolderHooker.kt
@@ -0,0 +1,51 @@
+package me.racci.raccicore.hooks
+
+import me.clip.placeholderapi.PlaceholderHook
+import org.bukkit.entity.Player
+
+//class PlaceHolderHooker {
+//
+//    private val REGISTERED_PLACEHOLDERS = HashMap<String, Placeholder>()
+//    private val REGISTERED_HOOKS = HashSet<PlaceholderHook>()
+//
+//    fun addHook(hook: PlaceholderHook) {
+//        hook.registerIntegration();
+//        REGISTERED_HOOKS.add(hook);
+//    }
+//
+//    fun registerPlaceholder(placeholder: PlaceholderEntry) {
+//        REGISTERED_PLACEHOLDERS.remove(placeholder.getIdentifier());
+//        REGISTERED_PLACEHOLDERS.put(placeholder.getIdentifier(), placeholder);
+//    }
+//
+//    fun getResult(identifier: String, player: Player? = null) : String {
+//        val placeholder = REGISTERED_PLACEHOLDERS[identifier.lowercase()] ?: return ""
+//        if(player == null && placeholder.requiresPlayer()) return ""
+//        return placeholder.getResult(player)
+//    }
+//
+//    fun translatePlaceholders(text: String, player: Player? = null): String {
+//        var processed = text
+//        for (integration in REGISTERED_HOOKS) {
+//            processed = integration.translate(processed, player)
+//        }
+//        return processed
+//    }
+//
+//}
+//
+//interface PlaceholderHook : Hook {
+//
+//    fun registerIntegration()
+//
+//    fun translate(text: String, player: Player? = null)
+//}
+//
+//class PlaceholderEntry {
+//
+//    private val identifier: String
+//    private val function: Function<Player, String>
+//    private val requiresPlayer: Boolean
+//
+//
+//}

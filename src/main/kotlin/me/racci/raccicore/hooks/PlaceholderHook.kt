3eb0e698b0df1957d29e93dfba0ea82642a93ac1
Da_Racci <tangentmoons@gmail.com>
2021-11-03 14:54:57 +1100

    WIP Hook service

---
+++ src/main/kotlin/me/racci/raccicore/hooks/PlaceholderHook.kt
@@ -0,0 +1,28 @@
+//package me.racci.raccicore.hooks
+//
+//import me.clip.placeholderapi.PlaceholderAPIPlugin
+//import me.clip.placeholderapi.expansion.PlaceholderExpansion
+//import me.racci.raccicore.RacciPlugin
+//
+//class PlaceholderHook : PlaceholderExpansion(), AbstractHookService<PlaceholderAPIPlugin> {
+//
+//    override val plugin: RacciPlugin = ;
+//    override fun getAuthor() = ;
+//    override fun getIdentifier() = ;
+//    override fun getVersion() = ;
+//
+//
+//
+//
+//    override fun persist() = true
+//
+//    override fun init() {
+//        TODO("Not yet implemented")
+//    }
+//
+//    override fun close() {
+//        TODO("Not yet implemented")
+//    }
+//
+//
+//}

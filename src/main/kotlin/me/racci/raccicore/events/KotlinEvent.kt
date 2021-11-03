35d07c11f30509f6f9c12874fc67903e659bb271
Da_Racci <tangentmoons@gmail.com>
2021-11-03 14:55:48 +1100

    Event fixes? i dont remmember what this was for

--- src/main/kotlin/me/racci/raccicore/events/KotlinEvent.kt
+++ src/main/kotlin/me/racci/raccicore/events/KotlinEvent.kt
@@ -1,91 +1,34 @@
 package me.racci.raccicore.events
 
-/**
- * Kotlin event
- *
- * @constructor
- *
- * @param async
- */
 abstract class KotlinEvent(async: Boolean = false): org.bukkit.event.Event(async), org.bukkit.event.Cancellable {
 
     private var cancelled = false
+    override fun isCancelled() = cancelled
+    override fun setCancelled(cancel: Boolean) {cancelled = cancel}
 
-    /**
-     * Set cancelled
-     *
-     * @param cancel
-     */
-    override fun setCancelled(cancel: Boolean) {
-        this.cancelled = true
-    }
-
-    /**
-     * Is cancelled
-     *
-     * @return
-     */
-    override fun isCancelled(): Boolean {
-        return cancelled
-    }
-
-    /**
-     * Get handlers
-     *
-     * @return
-     */
-    override fun getHandlers(): org.bukkit.event.HandlerList {
-        return handlerList
-    }
+    override fun getHandlers() = handlerList
 
     companion object {
         private val handlerList = org.bukkit.event.HandlerList()
 
         @JvmStatic
-        fun getHandlerList(): org.bukkit.event.HandlerList {
-            return handlerList
-        }
+        fun getHandlerList() = handlerList
     }
 }
 
 abstract class KPlayerEvent(player: org.bukkit.entity.Player, async: Boolean = false): org.bukkit.event.player.PlayerEvent(player, async), org.bukkit.event.Cancellable {
 
     private var cancelled = false
+    override fun isCancelled() = cancelled
+    override fun setCancelled(cancel: Boolean) {cancelled = cancel}
 
-    /**
-     * Set cancelled
-     *
-     * @param cancel
-     */
-    override fun setCancelled(cancel: Boolean) {
-        this.cancelled = true
-    }
-
-    /**
-     * Is cancelled
-     *
-     * @return
-     */
-    override fun isCancelled(): Boolean {
-        return cancelled
-    }
-
-    /**
-     * Get handlers
-     *
-     * @return
-     */
-    override fun getHandlers(): org.bukkit.event.HandlerList {
-        return handlerList
-    }
+    override fun getHandlers() = handlerList
 
     companion object {
         private val handlerList = org.bukkit.event.HandlerList()
 
         @JvmStatic
-        fun getHandlerList(): org.bukkit.event.HandlerList {
-            return handlerList
-        }
+        fun getHandlerList() = handlerList
     }
 }
 

d37dca33c6823725820e22325b71dfab68d54a4f
Da_Racci <tangentmoons@gmail.com>
2021-11-05 21:30:14 +1100

    Moved kyori methods

--- src/main/kotlin/me/racci/raccicore/utils/KotlinUtils.kt
+++ src/main/kotlin/me/racci/raccicore/utils/KotlinUtils.kt
@@ -2,19 +2,8 @@ package me.racci.raccicore.utils
 
 import me.racci.raccicore.RacciPlugin
 import me.racci.raccicore.utils.extensions.KotlinListener
-import net.kyori.adventure.text.BlockNBTComponent
-import net.kyori.adventure.text.Component
-import net.kyori.adventure.text.ComponentLike
-import net.kyori.adventure.text.EntityNBTComponent
-import net.kyori.adventure.text.KeybindComponent
-import net.kyori.adventure.text.ScoreComponent
-import net.kyori.adventure.text.SelectorComponent
-import net.kyori.adventure.text.StorageNBTComponent
-import net.kyori.adventure.text.TextComponent
-import net.kyori.adventure.text.TextReplacementConfig
-import net.kyori.adventure.text.TranslatableComponent
-import net.kyori.adventure.text.format.Style
-import net.kyori.adventure.util.RGBLike
+import org.bukkit.event.Event
+import org.bukkit.event.EventPriority
 
 inline fun <reified T : Throwable, reified U : Any> catch(
     err: (T) -> U,
@@ -35,8 +24,8 @@ inline fun <reified T : Throwable, reified U : Any> catch(
     run: () -> U
 ): U = catch<T, U>({ default }, run)
 
-inline fun <reified T : BukkitEvent> RacciPlugin.listen(
-    priority: BukkitEventPriority = BukkitEventPriority.NORMAL,
+inline fun <reified T : Event> RacciPlugin.listen(
+    priority: EventPriority = EventPriority.NORMAL,
     ignoreCancelled: Boolean = false,
     crossinline callback: (T) -> Unit
 ) = pm.registerEvent(T::class.java, object : KotlinListener {},
@@ -46,32 +35,3 @@ inline fun <reified T : BukkitEvent> RacciPlugin.listen(
 
 fun <T> T.not(other: T) = takeUnless { it == other }
 fun <T> T.notIn(container: Iterable<T>) = takeUnless { it in container }
-
-
-fun blockNBT(builder: BlockNBTComponent.Builder.() -> Unit): BlockNBTComponent = Component.blockNBT(builder)
-
-fun entityNBT(builder: EntityNBTComponent.Builder.() -> Unit): EntityNBTComponent = Component.entityNBT(builder)
-
-fun keybind(builder: KeybindComponent.Builder.() -> Unit): KeybindComponent = Component.keybind(builder)
-
-fun score(builder: ScoreComponent.Builder.() -> Unit): ScoreComponent = Component.score(builder)
-
-fun selector(builder: SelectorComponent.Builder.() -> Unit): SelectorComponent = Component.selector(builder)
-
-fun storageNBT(builder: StorageNBTComponent.Builder.() -> Unit): StorageNBTComponent = Component.storageNBT(builder)
-
-fun text(builder: TextComponent.Builder.() -> Unit): TextComponent = Component.text(builder)
-
-fun TextComponent.replace(builder: TextReplacementConfig.Builder.() -> Unit) = builder
-
-fun translatable(builder: TranslatableComponent.Builder.() -> Unit): TranslatableComponent = Component.translatable(builder)
-
-operator fun Component.plus(that: ComponentLike): Component = this.append(that)
-
-operator fun RGBLike.component1(): Int = this.red()
-
-operator fun RGBLike.component2(): Int = this.green()
-
-operator fun RGBLike.component3(): Int = this.blue()
-
-fun style(builder: Style.Builder.() -> Unit): Style = Style.style(builder)

1084798b19b55ce98a2d6b664efa9f5c48350117
Da_Racci <tangentmoons@gmail.com>
2021-11-05 21:29:58 +1100

    chore: Remove useless aliases

--- src/main/kotlin/me/racci/raccicore/utils/Aliases.kt
+++ src/main/kotlin/me/racci/raccicore/utils/Aliases.kt
@@ -1,9 +1,5 @@
 package me.racci.raccicore.utils
 
-
-typealias BukkitEvent = org.bukkit.event.Event
-typealias BukkitEventPriority = org.bukkit.event.EventPriority
-
 val server get() = org.bukkit.Bukkit.getServer()
 val console get() = org.bukkit.Bukkit.getConsoleSender()
 val pm get() = org.bukkit.Bukkit.getPluginManager()

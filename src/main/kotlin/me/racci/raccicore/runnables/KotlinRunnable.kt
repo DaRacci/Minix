b146c99f5dc48b3289b28b3b1f5fa00aacbe4463
Da_Racci <tangentmoons@gmail.com>
2021-11-05 21:28:53 +1100

    feat: more WIP stuff

--- src/main/kotlin/me/racci/raccicore/runnables/KotlinRunnable.kt
+++ src/main/kotlin/me/racci/raccicore/runnables/KotlinRunnable.kt
@@ -1,6 +1,7 @@
 package me.racci.raccicore.runnables
 
 import com.github.shynixn.mccoroutine.asyncDispatcher
+import com.github.shynixn.mccoroutine.launch
 import com.github.shynixn.mccoroutine.minecraftDispatcher
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Job
@@ -12,8 +13,12 @@ import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import me.racci.raccicore.RacciCore
 import me.racci.raccicore.RacciPlugin
+import me.racci.raccicore.utils.catch
 import org.bukkit.scheduler.BukkitRunnable
 import org.jetbrains.annotations.ApiStatus
+import java.util.concurrent.ConcurrentHashMap
+import java.util.concurrent.atomic.AtomicInteger
+import java.util.function.IntUnaryOperator
 
 //
 ///**
@@ -162,28 +167,31 @@ abstract class KotlinRunnable(
 ) : BukkitRunnable() {
 
     fun start() {
-        if(repeating) {
-            if(async) {
-                this.runTaskTimerAsynchronously(plugin, delay, period)
+        plugin.launch {
+            delay(delay)
+            if(repeating) {
+                if(async) {
+                    this@KotlinRunnable.runTaskTimerAsynchronously(plugin, delay, period)
+                } else {
+                    this@KotlinRunnable.runTaskTimer(plugin, delay, period)
+                }
             } else {
-                this.runTaskTimer(plugin, delay, period)
-            }
-        } else {
-            if(async) {
-                this.runTaskAsynchronously(plugin)
-            } else {
-                this.runTask(plugin)
+                if(async) {
+                    this@KotlinRunnable.runTaskAsynchronously(plugin)
+                } else {
+                    this@KotlinRunnable.runTask(plugin)
+                }
             }
         }
     }
 }
 
 suspend fun RacciPlugin.runnable(
-    async       : Boolean = false,
-    repeating   : Boolean = false,
-    delay       : Long = 0L,
-    period      : Long = 0L,
-    unit        : Unit.() -> Unit
+    async: Boolean = false,
+    repeating: Boolean = false,
+    delay: Long = 0L,
+    period: Long = 0L,
+    unit: Unit.()->Unit,
 ) = withContext(if(async) asyncDispatcher else minecraftDispatcher)
 {
 //    while()
@@ -202,10 +210,10 @@ suspend fun RacciPlugin.runnable(
  */
 @ApiStatus.Experimental
 class ConstantTask(
-    val plugin  : RacciPlugin,
-    val async   : Boolean,
-    val period  : Long,
-    val task    : CoroutineScope.() -> Unit
+    val plugin: RacciPlugin,
+    val async: Boolean,
+    val period: Long,
+    val task: CoroutineScope.()->Unit,
 ) {
 
     private var job: Job = CoroutineScope(if(async) plugin.asyncDispatcher else plugin.minecraftDispatcher).launch {
@@ -215,8 +223,6 @@ class ConstantTask(
         }
     }
 
-//    private testJob
-
     fun init() {
         job.start()
     }
@@ -225,12 +231,123 @@ class ConstantTask(
         job.
         job.cancelAndJoin()
     }
+
 }
 
-fun testTask() {
+object TaskManager {
+    /**
+     * The active and currently running tasks
+     */
+    val activeTasks = ConcurrentHashMap<Int, CoroutineTask>()
 
-    ConstantTask(RacciCore.instance, true, 20) {
+    /**
+     * Map for acquiring all of a plugins registered tasks
+     */
+    val pluginTaskMap = ConcurrentHashMap<RacciPlugin, ArrayList<Int>>()
+
+    /**
+     * ID Counter of tasks
+     */
+    private val ids = AtomicInteger(1)
+
+    /**
+     * Incrementer for task IDs, Yoinked from [org.bukkit.scheduler.BukkitScheduler]
+     */
+    private val INCREMENT_IDS = IntUnaryOperator label@ {
+        if(it == Int.MAX_VALUE) {
+            return@label 1
+        }
+        it + 1
+    }
+
+    fun init(
+        task: CoroutineTask,
+        delay: Long,
+    ) {
+        val id = ids.updateAndGet(INCREMENT_IDS)
+        activeTasks[id] = task
+        if(pluginTaskMap[task.owner] != null) {
+            pluginTaskMap[task.owner] = ArrayList()
+        }
+        pluginTaskMap[task.owner]!!.add(id)
+
+
 
     }
+
+    suspend fun close(plugin: RacciPlugin) = withContext(plugin.asyncDispatcher) {
+        val ids = pluginTaskMap[plugin]
+        if(ids != null) {
+            for(id in ids) {
+                close(id)
+            }
+        }
+    }
+
+    fun close(id: Int) = catch<Exception> {
+        if(activeTasks[id] != null) {
+            val task = activeTasks[id]!!
+            RacciCore.log.debug("Closing task $id owner - ${task.owner}")
+            task.cancel()
+            activeTasks.remove(id)
+        }
+    }
+
+
+}
+
+class CoroutineTask(
+    private val task: suspend Job.() -> Unit,
+    val owner: RacciPlugin,
+    val period: Long,
+    val async: Boolean,
+) {
+    val job: Job? = null
+    var cancelled: Boolean = false
+
+    fun init() {
+        CoroutineTask({
+
+        }, owner, period, async)
+    }
+
+    fun cancel() {
+        if(this.job != null) {
+            RacciCore.launchAsync {
+                cancelled = true
+                job.cancelAndJoin()
+            }
+        } else owner.log.debug("This task is not currently running!")
+    }
+
+
+
+}
+
+abstract class KotlinTask {
+    private val task: Job? = null
+
+    open val cancelled: Boolean
+        get() = task?.isCancelled ?: false
+
+    suspend fun close() {
+        task?.cancelAndJoin()
+    }
+
+    suspend fun initAsync(delay: Long = 0) = 0
+
+    suspend fun initSync(delay: Long = 0) = 0
+
+    suspend fun initConstantAsync(period: Long, delay: Long = 0) = 0
+
+    suspend fun initConstantSync(period: Long, delay: Long = 0) = 0
+
+    private suspend fun init(
+        async: Boolean,
+        repeating: Boolean,
+        period: Long,
+        delay: Long,
+    ) {}
+
 
 }

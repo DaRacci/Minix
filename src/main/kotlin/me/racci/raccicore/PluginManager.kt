9c9237a88b765b956b3d4e161551d0b41a11b2c4
Da_Racci <tangentmoons@gmail.com>
2021-11-07 18:15:51 +1100

    refactor: PluginManager

--- src/main/kotlin/me/racci/raccicore/PluginManager.kt
+++ src/main/kotlin/me/racci/raccicore/PluginManager.kt
@@ -4,6 +4,7 @@ import me.racci.raccicore.utils.extensions.pm
 import kotlin.reflect.KClass
 
 internal object PluginManager {
+
     val loadedPlugins = HashMap<KClass<out RacciPlugin>, RacciPlugin>()
 
     fun close() {
@@ -13,4 +14,5 @@ internal object PluginManager {
 
     fun add(plugin: RacciPlugin) {loadedPlugins[plugin::class] = plugin}
     fun remove(plugin: RacciPlugin) {loadedPlugins.remove(plugin::class)}
+
 }

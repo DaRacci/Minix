c29175926b6f99f58be31626e847551ab946221c
Da_Racci <tangentmoons@gmail.com>
2021-11-07 17:58:32 +1100

    Update internal PlayerManager.kt

--- src/main/kotlin/me/racci/raccicore/data/PlayerManager.kt
+++ src/main/kotlin/me/racci/raccicore/data/PlayerManager.kt
@@ -1,29 +1,37 @@
 package me.racci.raccicore.data
 
 import me.racci.raccicore.RacciCore
+import me.racci.raccicore.interfaces.IManager
 import me.racci.raccicore.utils.extensions.onlinePlayers
 import me.racci.raccicore.utils.listen
 import org.bukkit.event.player.PlayerJoinEvent
 import org.bukkit.event.player.PlayerQuitEvent
 import java.util.UUID
 
-internal object PlayerManager {
+internal object PlayerManager: IManager<PlayerManager> {
     private val playerData = HashMap<UUID, PlayerData>()
+    private var initialized = false
 
     operator fun get(uuid: UUID) =
         playerData[uuid]!!
 
-    fun init(plugin: RacciCore) {
-        onlinePlayers.forEach{PlayerData(it).init()}
-        plugin.listen<PlayerJoinEvent> {
-            PlayerData(it.player).init()
-        }
-        plugin.listen<PlayerQuitEvent> {
-            removePlayerData(it.player.uniqueId)
+    override fun init() {
+        val plugin = RacciCore.instance
+        if(!initialized) {
+            initialized = true
+            onlinePlayers.forEach{PlayerData(it).init()}
+            plugin.listen<PlayerJoinEvent> {
+                if(initialized) PlayerData(it.player).init()
+            }
+            plugin.listen<PlayerQuitEvent> {
+                if(initialized) removePlayerData(it.player.uniqueId)
+            }
+        } else {
+            plugin.log.error("PlayerManager has already been initialized")
         }
     }
 
-    fun close() {
+    override fun close() {
         playerData.clear()
     }
 

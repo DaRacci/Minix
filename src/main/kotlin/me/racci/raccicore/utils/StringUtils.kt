98403ac7372c1b56d26a90efe87784a32535247a
Da_Racci <tangentmoons@gmail.com>
2021-11-07 16:34:50 +1100

    refactor: Remove useless methods

--- src/main/kotlin/me/racci/raccicore/utils/StringUtils.kt
+++ src/main/kotlin/me/racci/raccicore/utils/StringUtils.kt
@@ -6,8 +6,6 @@ fun String.javaUnicodeToCharacter(): String = unicodeRegex.replace(this) {
     String(charArrayOf(it.destructured.component3().toInt(16).toChar()))
 }
 
-fun <T> T.print(): T = also { println(it) }
-
 fun String.centralize(
         length: Int,
         spacer: String = " ",
@@ -17,18 +15,4 @@ fun String.centralize(
     if (this.length >= length) return this
     val part = prefix + spacer.repeat((length - this.length) / 2) + suffix
     return part + this + part
-}
-
-val TRUE_CASES = arrayOf("true")
-    get() = field.clone()
-val FALSE_CASES = arrayOf("false")
-    get() = field.clone()
-
-fun String.toBooleanOrNull(
-    trueCases: Array<String> = TRUE_CASES,
-    falseCases: Array<String> = FALSE_CASES
-): Boolean? = when {
-    trueCases.any { it.equals(this, true) } -> true
-    falseCases.any { it.equals(this, true) } -> false
-    else -> null
 }

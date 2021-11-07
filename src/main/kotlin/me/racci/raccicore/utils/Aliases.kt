29d07d8fb7daef99c3b0791e8b2997928fb3bd51
Da_Racci <tangentmoons@gmail.com>
2021-11-07 16:30:42 +1100

    Moved to ExBukkit.kt

--- src/main/kotlin/me/racci/raccicore/utils/Aliases.kt
+++
@@ -1,6 +0,0 @@
-package me.racci.raccicore.utils
-
-val server get() = org.bukkit.Bukkit.getServer()
-val console get() = org.bukkit.Bukkit.getConsoleSender()
-val pm get() = org.bukkit.Bukkit.getPluginManager()
-

f508e2b17fc78a9270d9b2843855d77dee1c37c1
Da_Racci <tangentmoons@gmail.com>
2021-11-07 16:31:37 +1100

    style: Formatting

--- src/main/kotlin/me/racci/raccicore/utils/extensions/ExItemStack.kt
+++ src/main/kotlin/me/racci/raccicore/utils/extensions/ExItemStack.kt
@@ -2,4 +2,5 @@ package me.racci.raccicore.utils.extensions
 
 import org.bukkit.inventory.ItemStack
 
-val ItemStack.pdc get() = this.persistentDataContainer
+val ItemStack.pdc
+    get() = persistentDataContainer

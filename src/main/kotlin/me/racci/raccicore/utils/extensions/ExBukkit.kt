81d3af08cbeaa6866173e2420933c1a22a6d0ced
Da_Racci <tangentmoons@gmail.com>
2021-11-07 16:30:42 +1100

    refactor: Moved to ExBukkit.kt

--- src/main/kotlin/me/racci/raccicore/utils/extensions/ExBukkit.kt
+++ src/main/kotlin/me/racci/raccicore/utils/extensions/ExBukkit.kt
@@ -2,26 +2,19 @@ package me.racci.raccicore.utils.extensions
 
 import net.kyori.adventure.text.Component
 import org.bukkit.Bukkit
-import org.bukkit.Server
-import org.bukkit.command.ConsoleCommandSender
 import org.bukkit.entity.Player
 
-val server: Server get() = Bukkit.getServer()
+val server  get() = Bukkit.getServer()
+val console get() = Bukkit.getConsoleSender()
+val pm      get() = Bukkit.getPluginManager()
 
-object Console : ConsoleCommandSender by Bukkit.getConsoleSender() {
-    fun command(command: String) = Bukkit.dispatchCommand(this, command)
-}
-
-fun broadcast(component: Component) {
-    Bukkit.getServer().broadcast(component)
-}
+fun broadcast(component: Component) =
+    server.broadcast(component)
 
 fun broadcast(
-    players: Iterable<Player> = Bukkit.getOnlinePlayers(),
+    players: Iterable<Player> = onlinePlayers,
     component: Player.() -> Component
-) {
-    players.forEach(component::invoke)
-}
+) = players.forEach(component::invoke)
 
 fun Collection<Player>.broadcast(
     component: Player.() -> Component

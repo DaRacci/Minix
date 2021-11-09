21fe43987c4168ca8aa3edee1721eab2dc75ba01
Da_Racci <tangentmoons@gmail.com>
2021-11-09 16:48:20 +1100

    chore: Add javadocs

--- src/main/kotlin/me/racci/raccicore/interfaces/IFactory.kt
+++ src/main/kotlin/me/racci/raccicore/interfaces/IFactory.kt
@@ -2,10 +2,22 @@ package me.racci.raccicore.interfaces
 
 interface IFactory<T> {
 
+    /**
+     * This method should be used to populate any maps,
+     * variables and other info needed within the Factory.
+     */
     fun init()
 
+    /**
+     * This method should be used to repopulate maps,
+     * variables and other info with changes such as a Lang file change.
+     */
     fun reload()
 
+    /**
+     * This method should be used to clear maps,
+     * lists and leave as little behind as possible.
+     */
     fun close()
 
 }
--- src/main/kotlin/me/racci/raccicore/interfaces/IManager.kt
+++ src/main/kotlin/me/racci/raccicore/interfaces/IManager.kt
@@ -2,8 +2,15 @@ package me.racci.raccicore.interfaces
 
 interface IManager<T> {
 
+    /**
+     * This method should be used to populate any maps,
+     * variables and other info needed within the Manager.
+     */
     fun init()
 
+    /**
+     * This method should be used to clear maps,
+     * lists and leave as little behind as possible.
+     */
     fun close()
-
 }

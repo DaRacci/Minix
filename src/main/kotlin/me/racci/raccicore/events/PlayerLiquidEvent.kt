78a67ce30332d84533797606523482aebc62fff6
Da_Racci <tangentmoons@gmail.com>
2021-11-09 16:10:24 +1100

    refactor: javadoc and improve liquidType param

--- src/main/kotlin/me/racci/raccicore/events/PlayerLiquidEvent.kt
+++ src/main/kotlin/me/racci/raccicore/events/PlayerLiquidEvent.kt
@@ -1,29 +1,54 @@
 package me.racci.raccicore.events
 
 import org.bukkit.Location
+import org.bukkit.Material
+import org.bukkit.block.data.Waterlogged
+import org.bukkit.entity.Player
 
 /**
- * Player enter liquid event
+ * This event is fires when the player enters water, lava or a waterlogged block.
  *
- * @property player
- * @property liquidType
- * @property from
- * @property to
- * @constructor Create empty Player enter liquid event
+ * @property from The previous block where the player was before entering the liquid.
+ * @property to The liquid block the player has entered.
+ *
+ * @param player The player of the event.
+ * @param liquidType [Material.WATER] if it was water or a [Waterlogged] block,
+ * [Material.LAVA] if it was Lava
  */
 class PlayerEnterLiquidEvent(
-    val player: org.bukkit.entity.Player, val liquidType: Int, val from: Location, val to: Location
-    ) : KotlinEvent(true)
+    player: Player,
+    liquidType: Int,
+    val from: Location,
+    val to: Location
+): KPlayerEvent(player, true) {
+
+    val liquidType: Material =
+            if(liquidType == 1) {
+                Material.WATER
+            } else Material.LAVA
+
+}
 
 /**
- * Player exit liquid event
+ * This event is fires when the player exits water, lava or a waterlogged block.
  *
- * @property player
- * @property liquidType
- * @property from
- * @property to
- * @constructor Create empty Player exit liquid event
+ * @property from The liquid block that the player has exited.
+ * @property to The new non-liquid block.
+ *
+ * @param player The player of the event.
+ * @param liquidType [Material.WATER] if it was water or a [Waterlogged] block,
+ * [Material.LAVA] if it was Lava
  */
 class PlayerExitLiquidEvent(
-    val player: org.bukkit.entity.Player, val liquidType: Int, val from: Location, val to: Location
-    ) : KotlinEvent(true)
+    player: Player,
+    liquidType: Int,
+    val from: Location,
+    val to: Location
+): KPlayerEvent(player, true) {
+
+    val liquidType: Material =
+            if(liquidType == 1) {
+                Material.WATER
+            } else Material.LAVA
+
+}

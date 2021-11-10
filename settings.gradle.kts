6bcf70259791d4a7b06062e1f7aa1aeefaeb3bc1
Da_Racci <tangentmoons@gmail.com>
2021-11-10 17:37:59 +1100

    [SKIP] Build changes - Move off version catalog for now as its being really buggy - Disable Auto downloading of Kotlin and stuff as Bukkit is having the shits with me, and I need to figure it out later

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,21 +1,22 @@
 rootProject.name = "RacciCore"
-val USERNAME: String? by settings
-val TOKEN   : String? by settings
+//val USERNAME: String by settings
+//val TOKEN   : String by settings
 
-enableFeaturePreview("VERSION_CATALOGS")
-dependencyResolutionManagement {
-    repositories {
-        maven {
-            url = uri("https://maven.pkg.github.com/DaRacci/LibraryCatalog")
-            credentials {
-                username = System.getenv("USERNAME") ?: USERNAME
-                password = System.getenv("TOKEN")    ?: TOKEN
-            }
-        }
-    }
-    versionCatalogs {
-        create("libs") {
-            from("me.racci:librarycatalog:1.3")
-        }
-    }
-}
+//enableFeaturePreview("VERSION_CATALOGS")
+//dependencyResolutionManagement {
+//    repositories {
+//        mavenLocal()
+//        maven {
+//            url = uri("https://maven.pkg.github.com/DaRacci/LibraryCatalog")
+//            credentials {
+//                username = USERNAME //?: System.getenv("USERNAME")
+//                password = TOKEN    //?: System.getenv("TOKEN")
+//            }
+//        }
+//    }
+//    versionCatalogs {
+//        create("libs") {
+//            from("me.racci:LibraryCatalog:1.5")
+//        }
+//    }
+//}

f9b9902c2a58e81ec4a47858c380967c3ad9e9c8
Da_Racci <tangentmoons@gmail.com>
2021-11-10 17:41:01 +1100

    refactor: Rewrite the ComboEvents.kt again

--- src/main/kotlin/me/racci/raccicore/listeners/PlayerComboListener.kt
+++ src/main/kotlin/me/racci/raccicore/listeners/PlayerComboListener.kt
@@ -3,22 +3,13 @@ package me.racci.raccicore.listeners
 import kotlinx.coroutines.withContext
 import me.racci.raccicore.RacciCore
 import me.racci.raccicore.data.PlayerManager
-import me.racci.raccicore.events.PlayerDoubleOffhandEvent
-import me.racci.raccicore.events.PlayerLeftClickBlockEvent
-import me.racci.raccicore.events.PlayerLeftClickEntityEvent
+import me.racci.raccicore.events.AbstractComboEvent
+import me.racci.raccicore.events.BlockData
 import me.racci.raccicore.events.PlayerLeftClickEvent
-import me.racci.raccicore.events.PlayerOffhandEvent
-import me.racci.raccicore.events.PlayerRightClickBlockEvent
-import me.racci.raccicore.events.PlayerRightClickEntityEvent
 import me.racci.raccicore.events.PlayerRightClickEvent
-import me.racci.raccicore.events.PlayerShiftDoubleOffhandEvent
-import me.racci.raccicore.events.PlayerShiftLeftClickBlockEvent
-import me.racci.raccicore.events.PlayerShiftLeftClickEntityEvent
 import me.racci.raccicore.events.PlayerShiftLeftClickEvent
-import me.racci.raccicore.events.PlayerShiftOffhandEvent
-import me.racci.raccicore.events.PlayerShiftRightClickBlockEvent
-import me.racci.raccicore.events.PlayerShiftRightClickEntityEvent
 import me.racci.raccicore.events.PlayerShiftRightClickEvent
+import me.racci.raccicore.utils.ClassUtils
 import me.racci.raccicore.utils.extensions.KotlinListener
 import me.racci.raccicore.utils.extensions.pm
 import me.racci.raccicore.utils.now
@@ -34,33 +25,31 @@ import org.bukkit.event.player.PlayerSwapHandItemsEvent
 
 class PlayerComboListener : KotlinListener {
 
-    @EventHandler(priority = EventPriority.LOWEST, ignoreCancelled = true)
+    @EventHandler(priority = EventPriority.LOW, ignoreCancelled = true)
     suspend fun onInteract(
         event: PlayerInteractEvent,
     ) = withContext(RacciCore.asyncDispatcher) {
         if(event.action == Action.PHYSICAL) return@withContext
-        val player = event.player
 
-        val vEvent = when(event.action) {
-            Action.LEFT_CLICK_AIR    -> if(player.isSneaking) PlayerShiftLeftClickEvent(player, event.item) else PlayerLeftClickEvent(player, event.item)
-            Action.RIGHT_CLICK_AIR   -> if(player.isSneaking) PlayerShiftRightClickEvent(player, event.item) else PlayerRightClickEvent(player, event.item)
-            Action.LEFT_CLICK_BLOCK  -> if(player.isSneaking) PlayerShiftLeftClickBlockEvent(player, event.item, event.clickedBlock!!, event.blockFace) else PlayerLeftClickBlockEvent(player, event.item, event.clickedBlock!!, event.blockFace)
-            Action.RIGHT_CLICK_BLOCK -> if(player.isSneaking) PlayerShiftRightClickBlockEvent(player, event.item, event.clickedBlock!!, event.blockFace) else PlayerRightClickBlockEvent(player, event.item, event.clickedBlock!!, event.blockFace)
-            else                     -> return@withContext
-        }
+        val bd = if(event.clickedBlock != null) BlockData(event.clickedBlock!!, event.blockFace) else null
+        val s = if(event.player.isSneaking) "Shift" else ""
+        val c = if(event.action.isLeftClick) "Left" else "Right"
+
+        val clazz = Class.forName("me.racci.raccicore.events.Player${s}${c}ClickEvent") as Class<AbstractComboEvent>
+        val vEvent = ClassUtils.classConstructor(clazz.getConstructor(), event.player, event.item, bd, null)
 
         pm.callEvent(vEvent)
 
         if(vEvent.isCancelled) event.isCancelled = true
     }
 
-    @EventHandler(priority = EventPriority.LOWEST, ignoreCancelled = true)
+    @EventHandler(priority = EventPriority.LOW, ignoreCancelled = true)
     suspend fun onInteractAtEntity(
         event: PlayerInteractEntityEvent,
     ) = withContext(RacciCore.asyncDispatcher) {
         val vEvent = if(event.player.isSneaking) {
-            PlayerShiftRightClickEntityEvent(event.player, event.player.inventory.getItem(event.hand), event.rightClicked)
-        } else PlayerRightClickEntityEvent(event.player, event.player.inventory.getItem(event.hand), event.rightClicked)
+            PlayerShiftRightClickEvent(event.player, event.player.inventory.getItem(event.hand), null, event.rightClicked)
+        } else PlayerRightClickEvent(event.player, event.player.inventory.getItem(event.hand), null, event.rightClicked)
         pm.callEvent(vEvent)
 
         if(vEvent.isCancelled) event.isCancelled = true
@@ -73,8 +62,8 @@ class PlayerComboListener : KotlinListener {
         if(event.damager !is Player) return@withContext
         val p = event.damager as Player
         val vEvent = if(p.isSneaking) {
-            PlayerShiftLeftClickEntityEvent(p, p.inventory.itemInMainHand, event.entity)
-        } else PlayerLeftClickEntityEvent(p, p.inventory.itemInMainHand, event.entity)
+            PlayerShiftLeftClickEvent(p, p.inventory.itemInMainHand, null, event.entity)
+        } else PlayerLeftClickEvent(p, p.inventory.itemInMainHand, null, event.entity)
         pm.callEvent(vEvent)
 
         if(vEvent.isCancelled) event.isCancelled = true
@@ -85,19 +74,17 @@ class PlayerComboListener : KotlinListener {
     suspend fun onOffhand(
         event: PlayerSwapHandItemsEvent,
     ) = withContext(RacciCore.asyncDispatcher) {
-        val playerData = PlayerManager[event.player.uniqueId]
-        val last = playerData.lastOffhand
-        val player = event.player
-        val vEvent = (if((now().epochSeconds - last) <= 5L) {
-            if(player.isSneaking) {
-                PlayerShiftDoubleOffhandEvent(player, player.inventory.itemInMainHand, player.inventory.itemInOffHand)
-            } else PlayerDoubleOffhandEvent(player, player.inventory.itemInMainHand, player.inventory.itemInOffHand)
-        } else {
-            playerData.lastOffhand = now().epochSeconds
-            if(player.isSneaking) {
-                PlayerShiftOffhandEvent(player, player.inventory.itemInMainHand, player.inventory.itemInOffHand)
-            } else PlayerOffhandEvent(player, player.inventory.itemInMainHand, player.inventory.itemInOffHand)
-        })
+
+        val pd = PlayerManager[event.player.uniqueId]
+        val n = now().epochSeconds
+        val s = if(event.player.isSneaking) "Shift" else ""
+        val db = if(n - pd.lastOffhand <= 0.5) {
+            pd.lastOffhand = n
+            "Double"
+        } else ""
+
+        val clazz = Class.forName("me.racci.raccicore.events.Player${s}${db}OffhandEvent") as Class<AbstractComboEvent>
+        val vEvent = ClassUtils.classConstructor(clazz.getConstructor(), event.player, event.mainHandItem, event.offHandItem)
         pm.callEvent(vEvent)
 
         if(vEvent.isCancelled) event.isCancelled = true

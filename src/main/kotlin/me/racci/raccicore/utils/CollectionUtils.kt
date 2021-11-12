19f54072e63bad4092485812e6213d9942333ecf
Da_Racci <tangentmoons@gmail.com>
2021-11-12 17:04:45 +1100

    [SKIP] Update Javadoc and formatting for CollectionUtils.kt

--- src/main/kotlin/me/racci/raccicore/utils/CollectionUtils.kt
+++ src/main/kotlin/me/racci/raccicore/utils/CollectionUtils.kt
@@ -1,28 +1,79 @@
 package me.racci.raccicore.utils
 
-fun Collection<String>.containsIgnoreCase(
-        element: String
-): Boolean = any { it.equals(element, true) }
+object CollectionUtils {
 
-fun <T> MutableCollection<T>.clear(onRemove: (T) -> Unit) {
-    toMutableList().forEach {
-        remove(it)
-        onRemove(it)
+    /**
+     * Checks if the collection contains the [String] by IgnoreCase.
+     *
+     * @param element The [String] to look for.
+     * @return True if the collection contains the [String]
+     */
+    fun Collection<String>.containsIgnoreCase(
+        element: String
+    ): Boolean = any { it.equals(element, true) }
+
+    /**
+     * Checks if the array contains the [String] by IgnoreCase.
+     *
+     * @param element The [String] to look for.
+     * @return True if the array contains the [String]
+     */
+    fun Array<String>.containsIgnoreCase(
+        element: String
+    ): Boolean = any { it.equals(element, true) }
+
+    /**
+     * Checks if the map contains the [String] as a key by IgnoreCase.
+     *
+     * @param V The value Type.
+     * @param key The [String] to look for.
+     * @return True if the map contains the key of [String]
+     */
+    fun <V> Map<String, V>.containsKeyIgnoreCase(
+        key: String
+    ): Boolean = keys.containsIgnoreCase(key)
+
+    /**
+     * Attempts to find and retrieve the key matching the [String] by IgnoreCase.
+     *
+     * @param V The value Type.
+     * @param key The [String] to get.
+     * @return True if the map contains the key of [String]
+     */
+    fun <V> Map<String, V>.getIgnoreCase(
+        key: String
+    ): V? = entries.find { it.key.equals(key, true) }?.value
+
+    /**
+     * Compute an action with each item of this collection
+     * before removing it from the collection.
+     *
+     * @param T The Object Type.
+     * @param onRemove The Action to execute.
+     */
+    fun <T> MutableCollection<T>.clear(
+        onRemove: (T) -> Unit
+    ) {
+        toMutableList().forEach {
+            remove(it)
+            onRemove(it)
+        }
     }
-}
 
-fun Array<String>.containsIgnoreCase(
-        element: String
-): Boolean = any { it.equals(element, true) }
+    /**
+     * Compute an action with each entry of this map
+     * before removing it from the collection.
+     *
+     * @param K The key type.
+     * @param V The value type.
+     * @param onRemove The Action to execute.
+     */
+    fun <K, V> MutableMap<K, V>.clear(
+        onRemove: (K, V) -> Unit
+    ) {
+        keys.toMutableSet().forEach {
+            onRemove(it, remove(it)!!)
+        }
+    }
 
-fun <V> Map<String, V>.containsKeyIgnoreCase(
-        key: String
-): Boolean = keys.containsIgnoreCase(key)
-
-fun <V> Map<String, V>.getIgnoreCase(
-        key: String
-): V? = entries.find { it.key.equals(key, true) }?.value
-
-fun <K, V> MutableMap<K, V>.clear(onRemove: (K, V) -> Unit) {
-    keys.toMutableSet().forEach { onRemove(it, remove(it)!!) }
 }

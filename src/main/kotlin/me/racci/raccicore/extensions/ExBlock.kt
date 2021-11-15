8aa17fd832ec10a49287c545030d5d2ce0747ece
Da_Racci <tangentmoons@gmail.com>
2021-11-15 16:20:44 +1100

    [SKIP] Add more Extensions

---
+++ src/main/kotlin/me/racci/raccicore/extensions/ExBlock.kt
@@ -0,0 +1,42 @@
+package me.racci.raccicore.extensions
+
+import org.bukkit.Material
+import org.bukkit.block.Block
+import org.bukkit.block.data.BlockData
+import org.bukkit.entity.Player
+
+/**
+ * Send a block change to the [players]
+ * This block change is only visual and has
+ * no actual effect on the block.
+ *
+ * @param material The new [Material].
+ * @param data Extra data to apply to the [Block].
+ * @param players The players to see this change.
+ */
+fun Block.sendBlockChange(
+    material: Material,
+    data: Byte = 0,
+    vararg players: Player
+) {
+    players.filter{it.world.name == world.name}.forEach {
+        it.sendBlockChange(location, material, data)
+    }
+}
+
+/**
+ * Send a block change to the [players]
+ * This block change is only visual and has
+ * no actual effect on the block.
+ *
+ * @param blockData The new [BlockData].
+ * @param players The players to see this change.
+ */
+fun Block.sendBlockChange(
+    blockData: BlockData,
+    vararg players: Player
+) {
+    players.filter{it.world.name == world.name}.forEach {
+        it.sendBlockChange(location, blockData)
+    }
+}

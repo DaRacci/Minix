5849e3cf0c62f4f8e7912b9fcf0cd48238a84de9
Racci <tangentmoons@gmail.com>
2021-12-05 20:21:11 +1100

    Update ExMiniMessage.kt

--- src/main/kotlin/me/racci/raccicore/api/extensions/ExMiniMessage.kt
+++ src/main/kotlin/me/racci/raccicore/api/extensions/ExMiniMessage.kt
@@ -3,6 +3,7 @@ package me.racci.raccicore.api.extensions
 
 import net.kyori.adventure.text.Component
 import net.kyori.adventure.text.minimessage.MiniMessage
+import net.kyori.adventure.text.minimessage.template.TemplateResolver
 
 
 /**
@@ -14,8 +15,23 @@ import net.kyori.adventure.text.minimessage.MiniMessage
  * @return the output component
  * @since 0.3.0
  */
-inline fun miniMessage(
+inline fun MiniMessage.parse(
     input: String,
     italic: Boolean = false,
     builder: Component.() -> Unit = {}
-): Component = MiniMessage.get().parse(input).apply{if(!italic) noItalic()}.also(builder)
+): Component = parse(input).apply{if(!italic) noItalic()}.also(builder)
+
+/**
+ * Parses a string into a component.
+ *
+ * @param input The input string
+ * @param template Template pairs to resolve
+ * @param builder Block to apply to the component
+ * @return the output component
+ * @since 0.3.1
+ */
+inline fun MiniMessage.parse(
+    input: String,
+    vararg template: Pair<String, Any>,
+    builder: Component.() -> Unit = {}
+): Component = deserialize(input, TemplateResolver.pairs(template.toMap())).also(builder)

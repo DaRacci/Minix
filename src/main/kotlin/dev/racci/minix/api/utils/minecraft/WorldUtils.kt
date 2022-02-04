284854e9ae8e682d421e669211319064f23fbb8a
Racci <90304606+DaRacci@users.noreply.github.com>
2022-02-04 13:14:26 +1100

    refactor: Refactored the isDay utils and added isOverworld etc
    Signed-off-by: Racci <90304606+DaRacci@users.noreply.github.com>

--- src/main/kotlin/dev/racci/minix/api/utils/minecraft/WorldUtils.kt
+++ src/main/kotlin/dev/racci/minix/api/utils/minecraft/WorldUtils.kt
@@ -1,5 +1,3 @@
-@file:Suppress("UNUSED")
-
 package dev.racci.minix.api.utils.minecraft
 
 import org.bukkit.Location
@@ -8,30 +6,24 @@ import org.bukkit.entity.Player
 
 object WorldUtils {
 
-    fun isDay(player: Player): Boolean {
-        val time = player.world.time
-        return time < 13400 || time > 23400
-    }
-
     fun isDay(world: World): Boolean {
         val time = world.time
         return time < 13400 || time > 23400
     }
 
-    fun isDay(location: Location): Boolean {
-        val time = location.world.time
-        return time < 13400 || time > 23400
-    }
+    fun isDay(player: Player): Boolean = isDay(player.world)
 
-    fun isNight(player: Player): Boolean {
-        return !isDay(player)
-    }
+    fun isDay(location: Location): Boolean = isDay(location.world)
 
-    fun isNight(world: World): Boolean {
-        return !isDay(world)
-    }
+    fun isNight(player: Player): Boolean = !isDay(player)
 
-    fun isNight(location: Location): Boolean {
-        return !isDay(location)
-    }
+    fun isNight(world: World): Boolean = !isDay(world)
+
+    fun isNight(location: Location): Boolean = !isDay(location)
+
+    fun isOverworld(world: World): Boolean = world.environment == World.Environment.NORMAL
+
+    fun isNether(world: World): Boolean = world.environment == World.Environment.NETHER
+
+    fun isEnd(world: World): Boolean = world.environment == World.Environment.THE_END
 }

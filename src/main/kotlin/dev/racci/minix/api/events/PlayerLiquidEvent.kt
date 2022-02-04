d062e69802a71aa6d5ebfab4670c96c705a42ae9
Racci <90304606+DaRacci@users.noreply.github.com>
2022-02-04 23:00:04 +1100

    [ci-skip] Add some utility functions
    Signed-off-by: Racci <90304606+DaRacci@users.noreply.github.com>

--- src/main/kotlin/dev/racci/minix/api/events/PlayerLiquidEvent.kt
+++ src/main/kotlin/dev/racci/minix/api/events/PlayerLiquidEvent.kt
@@ -36,14 +36,19 @@ enum class LiquidType {
 
     companion object {
 
-        fun convert(block: Block) =
-            when (block.type) {
-                Material.WATER -> WATER
-                Material.LAVA -> LAVA
-                else ->
-                    if (block is Waterlogged &&
-                        block.isWaterlogged
-                    ) WATER else NON
-            }
+        fun convert(block: Block) = when (block.type) {
+            Material.WATER -> WATER
+            Material.LAVA -> LAVA
+            else -> if (block is Waterlogged && block.isWaterlogged) WATER else NON
+        }
+
+        fun convert(bucket: Material) = when (bucket) {
+            Material.WATER_BUCKET -> WATER
+            Material.LAVA_BUCKET -> LAVA
+            else -> NON
+        }
+
+        val Block.liquidType get() = convert(this)
+        val Material.liquidType get() = convert(this)
     }
 }

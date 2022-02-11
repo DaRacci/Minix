5d3187605923cbb77a6d309217c3aff3b5ea21e5
Racci <racci@racci.dev>
2022-02-11 23:59:31 +1100

    feat: Add docs and boolean returns for if the functions were invoked
    Signed-off-by: Racci <racci@racci.dev>

--- Minix-API/src/main/kotlin/dev/racci/minix/api/utils/kotlin/KotlinUtils.kt
+++ Minix-API/src/main/kotlin/dev/racci/minix/api/utils/kotlin/KotlinUtils.kt
@@ -37,6 +37,14 @@ infix fun KClass<*>.doesOverride(methodName: String): Boolean {
     return this.java.methods.find { it.name == methodName } in this.java.declaredMethods
 }
 
+/**
+ * Invokes the given block if the receiver overrides the supplied method.
+ *
+ * @param T The type of the receiver.
+ * @param methodName The name of the method to check for.
+ * @param block The block to invoke.
+ * @return If the block was invoked.
+ */
 inline fun <reified T : Any> T.invokeIfOverrides(
     methodName: String,
     block: (T) -> Unit
@@ -48,27 +56,60 @@ inline fun <reified T : Any> T.invokeIfOverrides(
     return false
 }
 
-fun <T> T.not(other: T) = takeUnless { it == other }
-fun <T> T.notIn(container: Iterable<T>) = takeUnless { it in container }
+/**
+ * Invokes the given block if the receiver is not null.
+ *
+ * @param T The type of the receiver.
+ * @param block The block to invoke.
+ * @return If the block was invoked.
+ */
+inline fun <T> T?.invokeIfNotNull(block: (T) -> Unit): Boolean = if (this != null) {
+    block(this)
+    true
+} else false
 
-inline fun <T> T?.invokeIfNotNull(block: (T) -> Unit): T? {
-    if (this != null) block(this)
+/**
+ * Invokes the given block if the receiver is null.
+ *
+ * @param T The type of the receiver.
+ * @param block The block to invoke.
+ * @return If the block was invoked
+ */
+inline fun <T> T?.invokeIfNull(block: () -> Unit): Boolean = if (this == null) {
+    block()
+    true
+} else false
+
+/**
+ * Invokes the given block if the boolean is true.
+ *
+ * @param block The block to invoke.
+ * @return If the block was invoked.
+ */
+inline fun Boolean?.ifTrue(block: () -> Unit): Boolean = if (this == true) {
+    block()
+    true
+} else false
+
+/**
+ * Invokes the given block if the boolean is false.
+ *
+ * @param block The block to invoke.
+ * @return If the block was invoked.
+ */
+inline fun Boolean?.ifFalse(block: () -> Unit): Boolean = if (this == false) {
+    block()
+    true
+} else false
+
+/**
+ * Invokes the given block if the receiver is not empty.
+ *
+ * @param T The value type of the collection.
+ * @param block The block to invoke.
+ * @return The collection itself.
+ */
+inline fun <reified T> Collection<T>?.ifNotEmpty(block: (Collection<T>) -> Unit): Collection<T>? {
+    if (this != null && this.isNotEmpty()) block(this)
     return this
-}
-
-inline fun <T> T?.invokeIfNull(block: () -> Unit): T? {
-    if (this == null) block()
-    return this
-}
-
-inline fun Boolean?.ifTrue(block: () -> Unit) {
-    if (this == true) block()
-}
-
-inline fun Boolean?.ifFalse(block: () -> Unit) {
-    if (this == false) block()
-}
-
-inline fun <reified T> Collection<T>.ifNotEmpty(block: (Collection<T>) -> Unit) {
-    if (this.isNotEmpty()) block(this)
 }

a368fa3400be1cd0b2829fe7f32f899409e9d1e4
Racci <racci@racci.dev>
2022-02-14 20:12:28 +1100

    feat(util): Add reflection clone util

--- Minix-API/src/main/kotlin/dev/racci/minix/api/utils/ReflectionUtils.kt
+++ Minix-API/src/main/kotlin/dev/racci/minix/api/utils/ReflectionUtils.kt
@@ -4,7 +4,11 @@ package dev.racci.minix.api.utils
 
 import dev.racci.minix.api.utils.kotlin.catchAndReturn
 import java.lang.reflect.Constructor
+import kotlin.reflect.KMutableProperty1
+import kotlin.reflect.KParameter
 import kotlin.reflect.KProperty1
+import kotlin.reflect.full.memberProperties
+import kotlin.reflect.full.primaryConstructor
 
 /**
  * Checks if a class exists.
@@ -41,3 +45,17 @@ inline fun <reified T> Any?.tryCast(block: T.() -> Unit): Boolean = if (this is 
     block()
     true
 } else false
+
+fun <T : Any> T.clone(replaceArgs: Map<KProperty1<T, *>, Any> = emptyMap()): T = javaClass.kotlin.run {
+    val consParams = primaryConstructor?.parameters ?: error("No primary constructor found")
+    val mutableProperties = memberProperties.filterIsInstance<KMutableProperty1<T, Any?>>()
+    val allValues = memberProperties
+        .filter { it in mutableProperties || it.name in consParams.map(KParameter::name) }
+        .associate { it.name to (replaceArgs[it] ?: it.get(this@clone)) }
+    // Safe to say this isn't null, because we checked it above
+    primaryConstructor!!.callBy(consParams.associateWith { allValues[it.name] }).also { newInstance ->
+        for (prop in mutableProperties) {
+            prop.set(newInstance, allValues[prop.name])
+        }
+    }
+}

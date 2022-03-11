58d80185db674a443dc9636ccf8ec75a04ecc0e2
Racci <racci@racci.dev>
2022-03-12 02:23:27 +1100

    chore: Delete CommandServices.kt as it is unused and i don't currently plan on expanding on it

--- Minix-API/src/main/kotlin/dev/racci/minix/api/extensions/ExCommand.kt
+++ Minix-API/src/main/kotlin/dev/racci/minix/api/extensions/ExCommand.kt
@@ -2,10 +2,6 @@
 
 package dev.racci.minix.api.extensions
 
-import dev.racci.minix.api.plugin.MinixPlugin
-import dev.racci.minix.api.services.CommandServices
-import dev.racci.minix.api.utils.kotlin.catch
-import org.bukkit.command.Command
 import org.bukkit.command.SimpleCommandMap
 import java.lang.reflect.Field
 
@@ -18,24 +14,5 @@ val serverCommands: SimpleCommandMap by lazy {
 val knownCommandsField: Field by lazy {
     SimpleCommandMap::class.java.getDeclaredField("knownCommands").apply {
         isAccessible = true
-    }
-}
-
-fun Command.register(plugin: MinixPlugin) {
-    serverCommands.register(plugin.name, this)
-    CommandServices.commands[plugin.name]?.add(this)
-}
-
-fun Command.unregister() {
-    catch<Exception> {
-        val knownCommands = knownCommandsField.get(serverCommands) as MutableMap<String, Command>
-        val toRemove = ArrayList<String>()
-        knownCommands
-            .filter { it.value == this }
-            .map(Map.Entry<String, Command>::key)
-            .forEach(toRemove::add)
-        toRemove.forEach(knownCommands::remove)
-        CommandServices.commands.values
-            .forEach { it.removeIf { i -> this === i } }
     }
 }

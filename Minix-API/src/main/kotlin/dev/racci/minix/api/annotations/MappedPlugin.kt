da8f6f23fd375977b5fca6c48b3497175d92c153
Racci <racci@racci.dev>
2022-03-24 01:49:40 +1100

    feat: Add MappedPlugin annotation for defining plugins

---
+++ Minix-API/src/main/kotlin/dev/racci/minix/api/annotations/MappedPlugin.kt
@@ -0,0 +1,21 @@
+package dev.racci.minix.api.annotations
+
+import dev.racci.minix.api.plugin.MinixPlugin
+import kotlin.reflect.KClass
+
+/**
+ * Marks a plugins information via an annotation instead of overriding the [MinixPlugin] interface fields.
+ *
+ * @property bStatsId The BStats ID of the plugin
+ * @property bindToKClass The class that this plugin binds to, if not specified, the plugin will bind to itself
+ * @property extensions The extensions that this plugin provides
+ * @property listeners The listeners that this plugin provides
+ */
+@Target(AnnotationTarget.CLASS)
+annotation class MappedPlugin(
+    val bStatsId: Int = -1,
+    val bindToKClass: KClass<*> = MinixPlugin::class,
+    val extensions: Array<KClass<*>> = [],
+    @Deprecated("Use an extension for listeners instead")
+    val listeners: Array<KClass<*>> = [],
+)

6b976d17af1ec7d37a5f8c9dd1cb989cfe647d70
Racci <racci@racci.dev>
2022-03-23 22:45:37 +1100

    fix: Remove useless and error causing field

--- Minix-API/src/main/kotlin/dev/racci/minix/api/plugin/PluginData.kt
+++ Minix-API/src/main/kotlin/dev/racci/minix/api/plugin/PluginData.kt
@@ -1,6 +1,5 @@
 package dev.racci.minix.api.plugin
 
-import dev.racci.minix.api.coroutine.contract.CoroutineService
 import dev.racci.minix.api.extension.Extension
 import dev.racci.minix.api.extension.ExtensionStateEvent
 import kotlinx.coroutines.flow.MutableSharedFlow
@@ -19,8 +18,6 @@ class PluginData<P : MinixPlugin>(val plugin: P) {
         thisRef: P,
         property: KProperty<*>,
     ): T = this::class.memberProperties.find { it.name == property.name }?.getter?.call(thisRef) as T
-
-    lateinit var coroutine: CoroutineService
 
     val extensionsBuilder by lazy(plugin::ExtensionsBuilder)
     val listenerBuilder by lazy(plugin::ListenerBuilder)

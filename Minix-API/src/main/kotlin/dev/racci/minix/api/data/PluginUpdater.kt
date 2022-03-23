95433661688db3d739f26bd784f03cb8c45e436d
Racci <racci@racci.dev>
2022-03-24 01:28:48 +1100

    fix: Configurate doesn't support lazy delegates :(

--- Minix-API/src/main/kotlin/dev/racci/minix/api/data/PluginUpdater.kt
+++ Minix-API/src/main/kotlin/dev/racci/minix/api/data/PluginUpdater.kt
@@ -11,33 +11,34 @@ import org.koin.core.component.KoinComponent
 import org.koin.core.component.get
 import org.spongepowered.configurate.objectmapping.ConfigSerializable
 import org.spongepowered.configurate.objectmapping.meta.Comment
-import kotlin.time.Duration
 
 @ConfigSerializable
-data class PluginUpdater(
+class PluginUpdater {
+
     @Comment("The name of the plugin")
-    var name: String,
+    var name: String? = null
+
     @Comment("Which update type to use")
-    var updateMode: UpdateMode = UpdateMode.UPDATE,
+    var updateMode: UpdateMode = UpdateMode.UPDATE
+
     @Comment("What providers are supported by this plugin")
-    var providers: Array<Pair<String, UpdateProvider>>,
+    var providers: MutableList<UpdateProvider> = mutableListOf()
+
     @Comment("What release channels should be updated to")
-    var channels: Array<String> = arrayOf("release"),
-    @Comment("How long between update checks")
-    var interval: Duration = updaterConfig.interval,
+    var channels: Array<String> = arrayOf("release")
+
     @Comment("Should we ignore any folders, or files when backing up this plugin?")
     var ignored: Array<String> = emptyArray()
-) {
 
     @Transient var failedAttempts: Int = 0
     @Transient var pluginInstance: Plugin? = null
     @Transient var lastRun: Instant? = null
     @Transient var remoteVersion: Version? = null
-    val localVersion: Version by lazy { Version(pluginInstance!!.description.version) } // By the time these are called the instance should be set.
-    val localFile: String by lazy { pluginInstance!!.description.version }
+    val localVersion: Version get() = Version(pluginInstance!!.description.version) // By the time these are called the instance should be set.
+    val localFile: String get() = pluginInstance!!.description.version
     @Transient var result: UpdateResult? = null
     @Transient var activeProvider: Int = 0
-    val provider: UpdateProvider get() = providers[activeProvider].second
+    val provider: UpdateProvider get() = providers[activeProvider]
 
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
@@ -47,7 +48,7 @@ data class PluginUpdater(
 
         if (name != other.name) return false
         if (updateMode != other.updateMode) return false
-        if (!providers.contentEquals(other.providers)) return false
+        if (providers != other.providers) return false
         if (!ignored.contentEquals(other.ignored)) return false
 
         return true
@@ -56,7 +57,7 @@ data class PluginUpdater(
     override fun hashCode(): Int {
         var result = name.hashCode()
         result = 31 * result + updateMode.hashCode()
-        result = 31 * result + providers.contentHashCode()
+        result = 31 * result + providers.hashCode()
         result = 31 * result + ignored.contentHashCode()
         return result
     }
--- Minix-API/src/main/kotlin/dev/racci/minix/api/data/UpdaterConfig.kt
+++ Minix-API/src/main/kotlin/dev/racci/minix/api/data/UpdaterConfig.kt
@@ -2,12 +2,12 @@ package dev.racci.minix.api.data
 
 import dev.racci.minix.api.annotations.MappedConfig
 import dev.racci.minix.api.plugin.Minix
-import dev.racci.minix.api.updater.providers.GitHubUpdateProvider
 import dev.racci.minix.api.utils.data.Data
 import org.spongepowered.configurate.objectmapping.ConfigSerializable
 import org.spongepowered.configurate.objectmapping.meta.Comment
+import org.spongepowered.configurate.objectmapping.meta.Setting
 import kotlin.time.Duration
-import kotlin.time.Duration.Companion.hours
+import kotlin.time.Duration.Companion.minutes
 
 @ConfigSerializable
 @MappedConfig(Minix::class, "updater.conf")
@@ -17,7 +17,11 @@ class UpdaterConfig {
     var enabled: Boolean = true
 
     @Comment("The interval in minutes to check for updates")
-    var interval: Duration = 1.hours
+    @Setting("interval")
+    private var _interval: Long = 60
+    var interval: Duration
+        get() = _interval.minutes
+        set(value) { _interval = value.inWholeMinutes }
 
     @Comment("What folder should we use for old files and backups?")
     var updateFolder: String = "MinixUpdater"
@@ -31,23 +35,31 @@ class UpdaterConfig {
     var backups: BackupConfig = BackupConfig()
 
     @Comment("Auto-updater providers for plugins")
-    var pluginUpdaters: MutableList<PluginUpdater> = mutableListOf(
-        PluginUpdater(
-            name = "Minix",
-            providers = arrayOf(
-                "Github" to GitHubUpdateProvider(
-                    projectOwner = "DaRacci",
-                    projectRepo = "Minix"
-                )
-            )
-        )
-    )
+    var pluginUpdaters: List<PluginUpdater> = emptyList()
 
     @ConfigSerializable
-    class BackupConfig(
+    class BackupConfig {
+
         @Comment("Should we create a backup of the plugins folder before updating?")
-        val enabled: Boolean = true,
+        val enabled: Boolean = true
+
         @Comment("What is the maximum size of a folder that is allowed to be backed up?")
         val maxSize: Data = Data.fromGigabytes(1)
-    )
+
+        override fun toString(): String {
+            return "BackupConfig(enabled=$enabled, maxSize=$maxSize)"
+        }
+    }
+
+    override fun toString(): String {
+        val builder = StringBuilder("UpdaterConfig:")
+        builder.append("enabled=$enabled:")
+        builder.append("interval=$interval:")
+        builder.append("updateFolder=$updateFolder:")
+        builder.append("announceDownloadProgress=$announceDownloadProgress:")
+        builder.append("downloadDependencies=$downloadDependencies:")
+        builder.append("backups=$backups:")
+        builder.append("pluginUpdaters=[${pluginUpdaters.joinToString(", ")}]:")
+        return builder.toString()
+    }
 }

5098e0f13633b85c97fbf9d66da9e37e3ab7acb4
Racci <racci@racci.dev>
2022-03-23 22:47:10 +1100

    refactor: Add finally block to UnsafeUtil.kt

--- Minix-API/src/main/kotlin/dev/racci/minix/api/utils/UnsafeUtil.kt
+++ Minix-API/src/main/kotlin/dev/racci/minix/api/utils/UnsafeUtil.kt
@@ -1,27 +1,35 @@
 package dev.racci.minix.api.utils
 
+import kotlinx.coroutines.runBlocking
 import kotlin.reflect.KClass
+
+private typealias Callback = suspend () -> Unit
 
 class UnsafeUtil {
 
-    private var dangerCallback: () -> Unit = { }
+    private var dangerCallback: Callback = { }
+    private var finallyCallback: Callback? = null
     private val exceptionHandlers = mutableMapOf<KClass<out Any>, Exception.() -> Unit>()
 
-    fun danger(callback: () -> Unit) {
+    fun danger(callback: Callback) {
         this.dangerCallback = callback
     }
 
     fun catch(
         vararg exceptionTypes: KClass<out Any>,
-        callback: Exception.() -> Unit = {},
+        callback: Exception.() -> Unit,
     ) {
         exceptionTypes.forEach {
             exceptionHandlers[it] = callback
         }
     }
 
+    fun finally(callback: Callback) {
+        this.finallyCallback = callback
+    }
+
     @Suppress("TooGenericExceptionCaught")
-    private fun execute() {
+    private suspend fun execute() {
         try {
             dangerCallback()
         } catch (ex: Exception) {
@@ -29,15 +37,16 @@ class UnsafeUtil {
                 .filter { (type, _) -> ex::class.simpleName == type.simpleName }
                 .forEach { it.value(ex) }
         }
+        finallyCallback?.invoke()
     }
 
     companion object {
 
         @Suppress("unused")
         fun Any.unsafe(callback: UnsafeUtil.() -> Unit) {
-            UnsafeUtil().apply {
+            with(UnsafeUtil()) {
                 callback()
-                execute()
+                runBlocking { execute() }
             }
         }
     }

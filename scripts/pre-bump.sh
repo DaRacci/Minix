d8b321e7da427d9ec44b336194238f816f407c25
Racci <racci@racci.dev>
2022-03-26 13:11:58 +1100

    fix(cog): Should be fixed i think

--- scripts/pre-bump.sh
+++ scripts/pre-bump.sh
@@ -1,10 +1,27 @@
 #!/usr/bin/env bash
 
+# Check if the temp file exists
+if [ ! -f "${TEMP_FILE}" ]; then
+  echo "Temp file does not exist. Exiting..."
+  exit 1
+fi
+
 PREVIOUS=$(grep -o '^[0-9]\+\.[0-9]\+\.[0-9]\+' temp | head -n 1)
 CURRENT=$(grep -o '[0-9]\+\.[0-9]\+\.[0-9]\+' temp | tail -n 1)
 
+# If previous version and version arent set then exit
+if [ -z "$PREVIOUS" ] || [ -z "$CURRENT" ]; then
+  echo "Previous version or current not set"
+  exit 1
+fi
+
+rm temp # Make sure we clean up after ourselves
+
 if ! ./gradlew test --info;
- then exit 1;
+ then
+   echo "Tests failed, aborting"
+   exit 1;
 fi
 
 echo "Bumping from v${PREVIOUS} to v${CURRENT}!"
+

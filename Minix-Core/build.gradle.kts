bd3b9939e3a44a18c3c4b7e1188a672938b324ff
Racci <racci@racci.dev>
2022-03-26 23:18:31 +1100

    chore: Forgot to add this to the commit for publishing the core dep

--- Minix-Core/build.gradle.kts
+++ Minix-Core/build.gradle.kts
@@ -1,4 +1,34 @@
+
+
 dependencies {
     compileOnly(project(":Minix-API"))
-    compileOnly("org.reflections:reflections:0.10.2")
+}
+
+java.withSourcesJar()
+
+tasks.dokkaHtml {
+    outputDirectory.set(file("/docs"))
+}
+
+tasks.withType<GenerateModuleMetadata> {
+    enabled = false
+}
+
+publishing {
+    repositories.maven("https://repo.racci.dev/releases") {
+        name = "RacciRepo"
+        credentials(PasswordCredentials::class)
+    }
+
+    publications.register("maven", MavenPublication::class) {
+        artifactId = rootProject.name + "-Core"
+        artifact(tasks.reobfJar.get().outputJar) {
+            builtBy(tasks.reobfJar)
+            classifier = null
+        }
+        artifact(tasks.kotlinSourcesJar) {
+            builtBy(tasks.kotlinSourcesJar)
+            classifier = "sources"
+        }
+    }
 }

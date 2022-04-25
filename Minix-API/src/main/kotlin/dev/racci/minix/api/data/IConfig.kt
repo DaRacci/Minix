f58f2e1abe2152ce945f20e7a3420b9c38947f58
DaRacci <racci@racci.dev>
2022-04-26 01:47:52 +1000

    fix: Transient values so we dont break configs

--- Minix-API/src/main/kotlin/dev/racci/minix/api/data/IConfig.kt
+++ Minix-API/src/main/kotlin/dev/racci/minix/api/data/IConfig.kt
@@ -6,11 +6,11 @@ abstract class IConfig {
 
     // TODO: Find a way to make this work
     /** A unit that is called whenever a property is modified. */
-    open val updateCallback: ((KProperty<*>, Any?) -> Unit)? = null
+    @Transient open val updateCallback: ((KProperty<*>, Any?) -> Unit)? = null
 
     /** A unit that is ran when this config is first loaded. */
-    open val loadCallback: (() -> Unit)? = null
+    @Transient open val loadCallback: (() -> Unit)? = null
 
     /** A unit that is ran when this config is unloaded. */
-    open val unloadCallback: (() -> Unit)? = null
+    @Transient open val unloadCallback: (() -> Unit)? = null
 }

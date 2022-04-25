02e48dfdc0f907d877a515f5fd201bdde80667f2
DaRacci <racci@racci.dev>
2022-04-26 01:03:37 +1000

    fix: make the vals open

--- Minix-API/src/main/kotlin/dev/racci/minix/api/data/IConfig.kt
+++ Minix-API/src/main/kotlin/dev/racci/minix/api/data/IConfig.kt
@@ -6,11 +6,11 @@ abstract class IConfig {
 
     // TODO: Find a way to make this work
     /** A unit that is called whenever a property is modified. */
-    val updateCallback: ((KProperty<*>, Any?) -> Unit)? = null
+    open val updateCallback: ((KProperty<*>, Any?) -> Unit)? = null
 
     /** A unit that is ran when this config is first loaded. */
-    val loadCallback: (() -> Unit)? = null
+    open val loadCallback: (() -> Unit)? = null
 
     /** A unit that is ran when this config is unloaded. */
-    val unloadCallback: (() -> Unit)? = null
+    open val unloadCallback: (() -> Unit)? = null
 }

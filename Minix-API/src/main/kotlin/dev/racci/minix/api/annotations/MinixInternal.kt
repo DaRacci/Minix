609fcab2437f83b518cddf3293244583040bd95b
DaRacci <racci@racci.dev>
2022-05-10 23:43:56 +1000

    chore: Add MinixInternal.kt annotation

---
+++ Minix-API/src/main/kotlin/dev/racci/minix/api/annotations/MinixInternal.kt
@@ -0,0 +1,8 @@
+package dev.racci.minix.api.annotations
+
+@RequiresOptIn(
+    level = RequiresOptIn.Level.ERROR,
+    message = "This API is internal in Minix and should not be used. It could be removed or changed without notice."
+)
+@Target(AnnotationTarget.CLASS, AnnotationTarget.CONSTRUCTOR, AnnotationTarget.FIELD, AnnotationTarget.FUNCTION, AnnotationTarget.PROPERTY, AnnotationTarget.PROPERTY_GETTER, AnnotationTarget.PROPERTY_SETTER)
+annotation class MinixInternal

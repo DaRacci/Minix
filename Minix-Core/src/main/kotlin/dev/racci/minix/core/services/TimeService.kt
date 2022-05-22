eff32ea38945eafc7e0b78b92434374e41fdae90
DaRacci <racci@racci.dev>
2022-05-23 01:42:21 +1000

    refactor(service): cache states are atomic

--- Minix-Core/src/main/kotlin/dev/racci/minix/core/services/TimeService.kt
+++ Minix-Core/src/main/kotlin/dev/racci/minix/core/services/TimeService.kt
@@ -5,48 +5,45 @@ import dev.racci.minix.api.coroutine.launch
 import dev.racci.minix.api.events.WorldDayEvent
 import dev.racci.minix.api.events.WorldNightEvent
 import dev.racci.minix.api.extension.Extension
-import dev.racci.minix.api.extensions.callEvent
 import dev.racci.minix.api.extensions.event
 import dev.racci.minix.api.extensions.onlinePlayers
 import dev.racci.minix.api.extensions.scheduler
 import dev.racci.minix.api.extensions.ticks
+import dev.racci.minix.api.extensions.world
 import dev.racci.minix.api.extensions.worlds
 import dev.racci.minix.api.plugin.Minix
-import dev.racci.minix.api.utils.collections.CollectionUtils.get
-import dev.racci.minix.api.utils.minecraft.WorldUtils.isDay
+import dev.racci.minix.api.utils.collections.CollectionUtils.cacheOf
+import kotlinx.atomicfu.AtomicBoolean
+import kotlinx.atomicfu.atomic
 import org.bukkit.World
 import org.bukkit.event.world.WorldLoadEvent
 import org.bukkit.event.world.WorldUnloadEvent
+import kotlin.reflect.full.primaryConstructor
 import kotlin.time.Duration.Companion.milliseconds
 
 @MappedExtension(Minix::class, "Time Service")
 class TimeService(override val plugin: Minix) : Extension<Minix>() {
 
-    private val timeState = HashMap<String, Boolean>()
+    private val timeState = cacheOf<String, AtomicBoolean> { atomic(world(this)!!.isDayTime) }
 
     override suspend fun handleEnable() {
 
         event<WorldLoadEvent> { checkTime(world) }
-        event<WorldUnloadEvent> { timeState -= world.name }
+        event<WorldUnloadEvent> { timeState.invalidate(world.name) }
 
-        scheduler { if (onlinePlayers.isNotEmpty()) worlds.forEach(::checkTime) }.runAsyncTaskTimer(plugin, 15.milliseconds, 5.ticks)
+        scheduler { if (onlinePlayers.isNotEmpty()) worlds.forEach(::checkTime) }.runAsyncTaskTimer(plugin, 15.milliseconds, 10.ticks)
     }
 
     private fun checkTime(world: World) {
-        val isDay = isDay(world)
-        val wasDay = timeState[world.name, !isDay]
+        val isDay = world.isDayTime
+        val wasDay = timeState[world.name].getAndSet(isDay)
 
         if (isDay == wasDay) return
 
-        timeState[world.name] = isDay
         val eventKClass = if (isDay) {
             WorldDayEvent::class
         } else WorldNightEvent::class
 
-        plugin.launch {
-            callEvent(eventKClass) {
-                mapOf(this[0] to world)
-            }
-        }
+        plugin.launch { eventKClass.primaryConstructor!!.call(world).callEvent() }
     }
 }

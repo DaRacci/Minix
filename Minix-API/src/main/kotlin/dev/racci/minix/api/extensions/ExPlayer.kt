4f3d5cb7abe56971bdad3d52cd86e7d13baaf55b
DaRacci <racci@racci.dev>
2022-05-23 01:49:50 +1000

    refactor!: Those aren't needed in ExPlayer.kt anymore as their now in ExWorld

--- Minix-API/src/main/kotlin/dev/racci/minix/api/extensions/ExPlayer.kt
+++ Minix-API/src/main/kotlin/dev/racci/minix/api/extensions/ExPlayer.kt
@@ -1,18 +1,7 @@
 package dev.racci.minix.api.extensions
 
 import dev.racci.minix.api.utils.minecraft.TameUtils
-import dev.racci.minix.api.utils.minecraft.WorldUtils
 import org.bukkit.entity.Entity
 import org.bukkit.entity.Player
 
 infix fun Player.isOwner(entity: Entity) = TameUtils.isOwner(this, entity)
-
-fun Player.isDay() = WorldUtils.isDay(this)
-
-fun Player.isNight() = WorldUtils.isNight(this)
-
-fun Player.inOverworld() = WorldUtils.isOverworld(this.world)
-
-fun Player.inNether() = WorldUtils.isNether(this.world)
-
-fun Player.inEnd() = WorldUtils.isEnd(this.world)

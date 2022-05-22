26793276d12aaab741b91a489af10e1b54c4517f
DaRacci <racci@racci.dev>
2022-05-23 01:45:57 +1000

    refactor!: Move extensionEvents to ExtensionState.kt

--- Minix-API/src/main/kotlin/dev/racci/minix/api/plugin/MinixPlugin.kt
+++ Minix-API/src/main/kotlin/dev/racci/minix/api/plugin/MinixPlugin.kt
@@ -3,15 +3,11 @@ package dev.racci.minix.api.plugin
 import dev.racci.minix.api.annotations.MappedPlugin
 import dev.racci.minix.api.annotations.MinixDsl
 import dev.racci.minix.api.data.PluginUpdater
-import dev.racci.minix.api.extension.Extension
-import dev.racci.minix.api.extension.ExtensionStateEvent
 import dev.racci.minix.api.services.PluginService
 import dev.racci.minix.api.utils.safeCast
-import kotlinx.coroutines.flow.filterIsInstance
 import org.bstats.bukkit.Metrics
 import org.bukkit.plugin.java.JavaPlugin
 import org.jetbrains.annotations.ApiStatus
-import org.jetbrains.annotations.ApiStatus.ScheduledForRemoval
 import org.koin.core.component.get
 import kotlin.reflect.KClass
 import kotlin.reflect.full.findAnnotation
@@ -50,19 +46,6 @@ abstract class MinixPlugin : JavaPlugin(), SusPlugin {
     @ApiStatus.NonExtendable
     override fun onLoad() {
         get<PluginService>().loadPlugin(this)
-    }
-
-    @ApiStatus.Internal
-    @ApiStatus.NonExtendable
-    suspend inline fun send(event: ExtensionStateEvent) {
-        get<PluginService>()[this].extensionEvents.emit(event)
-    }
-
-    // TODO: Is this how this works?
-    suspend inline fun <reified T : Extension<P>, P : MinixPlugin> extensionEvent(crossinline block: suspend T.() -> Unit) {
-        get<PluginService>()[this].extensionEvents
-            .filterIsInstance<T>()
-            .collect { block.invoke(it) }
     }
 
     @MinixDsl

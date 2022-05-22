1cfe1f66e32c39b6560e0790b21fa228509b8cd2
DaRacci <racci@racci.dev>
2022-05-23 01:58:22 +1000

    refactor(extension): Remove deprecated for 3.0.0

--- Minix-API/src/main/kotlin/dev/racci/minix/api/extension/Extension.kt
+++ Minix-API/src/main/kotlin/dev/racci/minix/api/extension/Extension.kt
@@ -2,8 +2,7 @@ package dev.racci.minix.api.extension
 
 import dev.racci.minix.api.annotations.MappedExtension
 import dev.racci.minix.api.annotations.MinixInternal
-import dev.racci.minix.api.coroutine.launch
-import dev.racci.minix.api.coroutine.launchAsync
+import dev.racci.minix.api.extensions.SimpleKListener
 import dev.racci.minix.api.extensions.WithPlugin
 import dev.racci.minix.api.extensions.inWholeTicks
 import dev.racci.minix.api.plugin.Minix
@@ -17,11 +16,9 @@ import kotlinx.collections.immutable.ImmutableList
 import kotlinx.collections.immutable.toImmutableList
 import kotlinx.coroutines.CompletableDeferred
 import net.minecraft.world.entity.ai.memory.ExpirableValue
-import org.jetbrains.annotations.ApiStatus.ScheduledForRemoval
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
 import org.koin.core.qualifier.Qualifier
-import java.util.concurrent.CompletableFuture
 import kotlin.reflect.KClass
 import kotlin.reflect.KProperty
 import kotlin.reflect.full.findAnnotation
@@ -67,85 +64,9 @@ abstract class Extension<P : MinixPlugin> : KoinComponent, Qualifier, WithPlugin
     open suspend fun handleUnload() {}
 
     open suspend fun setState(state: ExtensionState) {
-        minix.send(ExtensionStateEvent(this, state))
+        send(plugin, ExtensionStateEvent(this, state))
         this.state = state
     }
-
-    @ScheduledForRemoval(inVersion = "3.0.0")
-    @Deprecated("Moved to ExPlugin", ReplaceWith("WithPlugin<*>.sync(suspend () -> R)", "dev.racci.minix.api.extension.sync"))
-    inline fun <reified R> sync(crossinline block: suspend () -> R) { plugin.launch() { block() } }
-
-    @ScheduledForRemoval(inVersion = "3.0.0")
-    @Deprecated("Moved to ExPlugin", ReplaceWith("WithPlugin<*>.async(suspend () -> R)", "dev.racci.minix.api.extension.async"))
-    inline fun <reified R> async(crossinline block: suspend () -> R) { plugin.launchAsync { block() } }
-
-    @ScheduledForRemoval(inVersion = "3.0.0")
-    @Deprecated("Moved to ExPlugin", ReplaceWith("WithPlugin<*>.completableSync(suspend () -> R)", "dev.racci.minix.api.extension.completableSync"))
-    inline fun <reified R> completableSync(crossinline block: suspend () -> R): CompletableFuture<R> {
-        val future = CompletableFuture<R>()
-        plugin.launch { future.complete(block()) }.invokeOnCompletion {
-            if (it == null) return@invokeOnCompletion
-            future.cancel(true)
-        }
-        return future
-    }
-
-    @ScheduledForRemoval(inVersion = "3.0.0")
-    @Deprecated("Moved to ExPlugin", ReplaceWith("WithPlugin<*>.completableAsync(suspend () -> R)", "dev.racci.minix.api.extension.completableAsync"))
-    inline fun <reified R> completableAsync(crossinline block: suspend () -> R): CompletableFuture<R> {
-        val future = CompletableFuture<R>()
-        plugin.launchAsync { future.complete(block()) }.invokeOnCompletion {
-            if (it == null) return@invokeOnCompletion
-            future.cancel(true)
-        }
-        return future
-    }
-
-    @ScheduledForRemoval(inVersion = "3.0.0")
-    @Deprecated("Moved to ExPlugin", ReplaceWith("WithPlugin<*>.deferredSync(suspend () -> R)", "dev.racci.minix.api.extension.deferredSync"))
-    inline fun <reified R> deferredSync(crossinline block: suspend () -> R): CompletableDeferred<R> {
-        val deferred = CompletableDeferred<R>()
-        plugin.launch { deferred.complete(block()) }.invokeOnCompletion {
-            if (it == null) return@invokeOnCompletion
-            deferred.cancel()
-        }
-        return deferred
-    }
-
-    @ScheduledForRemoval(inVersion = "3.0.0")
-    @Deprecated("Moved to ExPlugin", ReplaceWith("WithPlugin<*>.deferredAsync(suspend () -> R)", "dev.racci.minix.api.extension.deferredAsync"))
-    inline fun <reified R> deferredAsync(crossinline block: suspend () -> R): CompletableDeferred<R> {
-        val deferred = CompletableDeferred<R>()
-        plugin.launchAsync { deferred.complete(block()) }.invokeOnCompletion {
-            if (it == null) return@invokeOnCompletion
-            deferred.cancel()
-        }
-        return deferred
-    }
-
-    @ScheduledForRemoval(inVersion = "3.0.0")
-    @Deprecated("Removed in 3.0.0", ReplaceWith("WithPlugin<*>.sync(suspend () -> Unit)", "dev.racci.minix.api.extension.sync"))
-    inline fun <reified T : () -> R, reified R> T.sync() = sync(this)
-
-    @ScheduledForRemoval(inVersion = "3.0.0")
-    @Deprecated("Removed in 3.0.0", ReplaceWith("WithPlugin<*>.async(suspend () -> Unit)", "dev.racci.minix.api.extension.async"))
-    inline fun <reified T : () -> R, reified R> T.async() = async(this)
-
-    @ScheduledForRemoval(inVersion = "3.0.0")
-    @Deprecated("Removed in 3.0.0", ReplaceWith("WithPlugin<*>.completableSync(suspend () -> Unit)", "dev.racci.minix.api.extension.completableSync"))
-    inline fun <reified T : () -> R, reified R> T.completableSync(): CompletableFuture<R> = completableSync(this)
-
-    @ScheduledForRemoval(inVersion = "3.0.0")
-    @Deprecated("Removed in 3.0.0", ReplaceWith("WithPlugin<*>.completableAsync(suspend () -> Unit)", "dev.racci.minix.api.extension.completableAsync"))
-    inline fun <reified T : () -> R, reified R> T.completableAsync(): CompletableFuture<R> = completableAsync(this)
-
-    @ScheduledForRemoval(inVersion = "3.0.0")
-    @Deprecated("Removed in 3.0.0", ReplaceWith("WithPlugin<*>.deferredSync(suspend () -> Unit)", "dev.racci.minix.api.extension.deferredSync"))
-    inline fun <reified T : () -> R, reified R> T.deferredSync(): CompletableDeferred<R> = deferredSync(this)
-
-    @ScheduledForRemoval(inVersion = "3.0.0")
-    @Deprecated("Removed in 3.0.0", ReplaceWith("WithPlugin<*>.deferredAsync(suspend () -> Unit)", "dev.racci.minix.api.extension.deferredAsync"))
-    inline fun <reified T : () -> R, reified R> T.deferredAsync(): CompletableDeferred<R> = deferredAsync(this)
 
     final override fun toString(): String {
         return "${plugin.name}:$value"

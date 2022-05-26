a8a7b8b3b46edbc4726a9406086ea37ef489733f
DaRacci <racci@racci.dev>
2022-05-27 01:44:12 +1000

    refactor(api): use better method? idk i know what it does exactly now though

--- Minix-API/src/main/kotlin/dev/racci/minix/api/utils/FileSystemUtils.kt
+++ Minix-API/src/main/kotlin/dev/racci/minix/api/utils/FileSystemUtils.kt
@@ -1,6 +1,7 @@
 package dev.racci.minix.api.utils
 
 import java.io.File
+import java.io.IOException
 import java.nio.file.Path
 
 object FileSystemUtils : UtilObject by UtilObject {
@@ -10,12 +11,35 @@ object FileSystemUtils : UtilObject by UtilObject {
         maxDepth: Int = Int.MAX_VALUE
     ): Long {
         var size = 0L
-        file.walkTopDown().onEnter {
-            size += it.length()
-            true
-        }.onFail { f, e ->
-            minix.log.debug(e) { "Could not enter file [${f.path}]." }
-        }.maxDepth(maxDepth) // .sumOf { it.length() }
+
+        if (file.isFile) {
+            return file.length()
+        }
+
+        if (!file.isDirectory) return 0
+
+        val folders = mutableListOf<File>()
+        var depth = 0
+        val itr = file.listFiles()?.iterator() ?: return 0
+
+        while (folders.isNotEmpty() || itr.hasNext()) {
+            try {
+                val next = folders.getOrNull(0)?.also {
+                    folders.removeAt(0)
+                } ?: run { depth = 0; itr.next() }
+
+                when {
+                    depth >= maxDepth -> { /* Skip this folder */ }
+                    next.isDirectory -> {
+                        depth++
+                        folders.addAll(next.listFiles() ?: continue)
+                    }
+                    else -> size += next.length()
+                }
+            } catch (e: IOException) {
+                minix.log.debug(e) { "Could not enter file" }
+            }
+        }
         return size
     }
 }

e546028b40466ce0c67a6526359e0121f150b868
DaRacci <racci@racci.dev>
2022-05-26 13:52:02 +1000

    refactor: All leading to single methods

--- Minix-API/src/main/kotlin/dev/racci/minix/api/extensions/ExPlugin.kt
+++ Minix-API/src/main/kotlin/dev/racci/minix/api/extensions/ExPlugin.kt
@@ -10,12 +10,14 @@ import dev.racci.minix.api.extension.Extension
 import dev.racci.minix.api.plugin.MinixPlugin
 import dev.racci.minix.api.utils.safeCast
 import kotlinx.coroutines.CompletableDeferred
+import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Deferred
 import kotlinx.coroutines.Job
 import org.bukkit.event.Listener
 import org.bukkit.plugin.Plugin
 import java.util.concurrent.CompletableFuture
+import kotlin.coroutines.CoroutineContext
 
 /** Registers all of these listeners for the plugin. */
 fun Plugin.registerEvents(
@@ -40,31 +42,41 @@ inline fun WithPlugin<*>.async(noinline block: suspend CoroutineScope.() -> Unit
 }
 
 /** Returns a [CompletableFuture] that is completed when suspended lambda is completed */
-inline fun <R> WithPlugin<*>.completableSync(crossinline block: suspend () -> R): CompletableFuture<R> {
-    val future = CompletableFuture<R>()
-    sync { future.complete(block()) }.invokeOnCompletion(future::completeExceptionally)
-    return future
-}
+inline fun <R> WithPlugin<*>.completableSync(crossinline block: suspend () -> R): CompletableFuture<R> = completable(plugin.minecraftDispatcher, block)
 
 /** Returns a [CompletableFuture] that is completed when suspended lambda is completed */
-inline fun <R> WithPlugin<*>.completableAsync(crossinline block: suspend () -> R): CompletableFuture<R> {
+inline fun <R> WithPlugin<*>.completableAsync(crossinline block: suspend () -> R): CompletableFuture<R> = completable(plugin.asyncDispatcher, block)
+
+/** Returns a [Deferred] that is completed when suspended lambda is completed */
+inline fun <R> WithPlugin<*>.deferredSync(crossinline block: suspend () -> R): Deferred<R> = deferred(plugin.minecraftDispatcher, block)
+
+/** Returns a [Deferred] that is completed when suspended lambda is completed */
+inline fun <R> WithPlugin<*>.deferredAsync(crossinline block: suspend () -> R): Deferred<R> = deferred(plugin.asyncDispatcher, block)
+
+inline fun <R> WithPlugin<*>.deferred(
+    dispatcher: CoroutineContext,
+    crossinline block: suspend () -> R
+): Deferred<R> {
+    val deferred = CompletableDeferred<R>()
+    launch(dispatcher) { deferred.complete(block()) }.invokeOnCompletion { it?.let(deferred::completeExceptionally) }
+    return deferred
+}
+
+inline fun <R> WithPlugin<*>.completable(
+    dispatcher: CoroutineContext,
+    crossinline block: suspend () -> R
+): CompletableFuture<R> {
     val future = CompletableFuture<R>()
-    async { future.complete(block()) }.invokeOnCompletion(future::completeExceptionally)
+    launch(dispatcher) { future.complete(block()) }.invokeOnCompletion(future::completeExceptionally)
     return future
 }
 
-/** Returns a [Deferred] that is completed when suspended lambda is completed */
-inline fun <R> WithPlugin<*>.deferredSync(crossinline block: suspend () -> R): Deferred<R> {
-    val deferred = CompletableDeferred<R>()
-    sync { deferred.complete(block()) }.invokeOnCompletion { it?.let(deferred::completeExceptionally) }
-    return deferred
-}
-
-/** Returns a [Deferred] that is completed when suspended lambda is completed */
-inline fun <R> WithPlugin<*>.deferredAsync(crossinline block: suspend () -> R): Deferred<R> {
-    val deferred = CompletableDeferred<R>()
-    async { deferred.complete(block()) }.invokeOnCompletion { it?.let(deferred::completeExceptionally) }
-    return deferred
+fun WithPlugin<*>.launch(
+    dispatcher: CoroutineContext,
+    block: suspend CoroutineScope.() -> Unit
+): Job {
+    val parent = this.safeCast<Extension<*>>()?.supervisor
+    return plugin.launch(dispatcher, parent, block)
 }
 
 interface WithPlugin<T : MinixPlugin> {

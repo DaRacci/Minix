8881b32d3e31cabfbecc4ac59641e051118071b3
DaRacci <racci@racci.dev>
2022-05-27 01:04:07 +1000

    test(api): implement test for Closable

---
+++ Minix-API/src/test/kotlin/dev/racci/minix/api/utils/CloseableTest.kt
@@ -0,0 +1,35 @@
+package dev.racci.minix.api.utils
+
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
+import kotlin.reflect.full.memberProperties
+import kotlin.reflect.jvm.isAccessible
+import kotlin.test.assertNull
+import kotlin.test.assertTrue
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+internal class CloseableTest {
+
+    private var closeable: Closeable<Boolean>? = null
+
+    @BeforeAll
+    fun setup() {
+        closeable = object : Closeable<Boolean>() {
+            override fun create(): Boolean {
+                return true
+            }
+        }
+    }
+
+    @Test
+    fun `closeable should be created when gotten`() {
+        assertTrue(closeable!!.get())
+    }
+
+    @Test
+    fun `closeable should false once closed`() {
+        closeable!!.close()
+        assertNull(closeable!!::class.memberProperties.first { it.name == "value" }.also { it.isAccessible = true }.call(closeable!!))
+    }
+}

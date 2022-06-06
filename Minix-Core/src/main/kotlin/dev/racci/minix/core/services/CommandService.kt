62c343e6bc05fc9f74c544a173a4bd4ea556ba88
DaRacci <racci@racci.dev>
2022-06-06 10:42:55 +1000

    fix(service): Unmap CommandService as its unused for now

--- Minix-Core/src/main/kotlin/dev/racci/minix/core/services/CommandService.kt
+++ Minix-Core/src/main/kotlin/dev/racci/minix/core/services/CommandService.kt
@@ -4,5 +4,4 @@ import dev.racci.minix.api.annotations.MappedExtension
 import dev.racci.minix.api.extension.Extension
 import dev.racci.minix.api.plugin.Minix
 
-@MappedExtension(Minix::class, "Command Service")
 class CommandService(override val plugin: Minix) : Extension<Minix>()

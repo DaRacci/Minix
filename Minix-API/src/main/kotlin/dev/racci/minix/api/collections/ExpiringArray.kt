8690d3e82d91be6585106d8fbfe9a4014c92ef2d
DaRacci <racci@racci.dev>
2022-08-07 20:38:24 -0700

    feat: ExpiringArray
    Automatically removes items from the array after they have
    expired.
    Created for use such as a cooldown.

---
+++ Minix-API/src/main/kotlin/dev/racci/minix/api/collections/ExpiringArray.kt
@@ -0,0 +1,30 @@
+package dev.racci.minix.api.collections
+
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.runBlocking
+import kotlin.time.Duration
+
+class ExpiringArray<T>(val duration: Duration) : HashSet<T>() {
+
+    /**
+     * Attempts to add this element to the queue.
+     * If the element is already part of the queue,
+     * And the cooldown hasn't expired, the cooldown remains unchanged.
+     *
+     * @param element The element to add to the queue.
+     * @return True, if not present or if the cooldown has expired, otherwise else.
+     */
+    override fun add(element: T): Boolean {
+        if (contains(element)) return false
+
+        val added = super.add(element)
+        if (!added) return false
+
+        runBlocking {
+            delay(duration)
+            remove(element)
+        }
+
+        return true
+    }
+}

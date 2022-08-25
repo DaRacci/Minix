b871075adffcd6d2756295f1670f533b34ddd3b1
DaRacci <racci@racci.dev>
2022-08-25 14:13:24 -0700

    fix: fix minix config

--- Minix-Core/src/main/kotlin/dev/racci/minix/core/data/MinixConfig.kt
+++ Minix-Core/src/main/kotlin/dev/racci/minix/core/data/MinixConfig.kt
@@ -1,9 +1,8 @@
 package dev.racci.minix.core.data
 
 import dev.racci.minix.api.annotations.MappedConfig
+import dev.racci.minix.api.data.MinixConfig
 import dev.racci.minix.api.plugin.Minix
-import dev.racci.minix.api.plugin.logger.MinixLogger
-import org.koin.core.component.KoinComponent
 import org.koin.core.component.get
 import org.spongepowered.configurate.objectmapping.ConfigSerializable
 import org.spongepowered.configurate.objectmapping.meta.Comment
@@ -12,21 +11,11 @@ import java.util.UUID
 
 @ConfigSerializable
 @MappedConfig(Minix::class, "config.conf")
-class MinixConfig : KoinComponent {
+class MinixConfig : MinixConfig<Minix>() {
 
     @Setting("server-uuid")
     @Comment("This server unique uuid (Please don't change this)")
     var serverUUID: UUID = UUID.randomUUID()
-
-    @Comment("What log level should be used? [FATAL, ERROR, WARNING, INFO, DEBUG, TRACE]")
-    var loggingLevel: String = MinixLogger.LoggingLevel.INFO.name
-        set(value) {
-            val uppercase = value.uppercase()
-            val logger = get<MinixLogger>()
-            val level = MinixLogger.LoggingLevel.values().find { it.name == uppercase } ?: return logger.warn { "Invalid logging level $value" }
-            field = value
-            getKoin().get<Minix>().log.setLevel(level)
-        }
 
     @Comment(
         """

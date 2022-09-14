79c0b2581ddf4c88f323fd05a26a62e27f9fcf7d
DaRacci <racci@racci.dev>
2022-09-14 15:48:25 +1000

    chore: Redundant deprecation

--- Minix-API/src/main/kotlin/dev/racci/minix/api/extensions/ExString.kt
+++ Minix-API/src/main/kotlin/dev/racci/minix/api/extensions/ExString.kt
@@ -7,6 +7,5 @@ fun String.toNamespacedKey(): NamespacedKey {
     assert(split.size == 2) {
         "Malformed key: $this, a key must only contain a singular : that splits the namespaced from the key."
     }
-    @Suppress("Deprecation") // lmao fuck you imma use what is meant to be an internal function
     return NamespacedKey(split[0], split[1])
 }

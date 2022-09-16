9304eee8a0291539cf55e62f377239f1c9703479
DaRacci <racci@racci.dev>
2022-09-17 00:50:18 +1000

    refactor(UtilObject): Add logger function

--- Minix-API/src/main/kotlin/dev/racci/minix/api/utils/UtilObject.kt
+++ Minix-API/src/main/kotlin/dev/racci/minix/api/utils/UtilObject.kt
@@ -1,12 +1,16 @@
 package dev.racci.minix.api.utils
 
 import dev.racci.minix.api.plugin.MinixPlugin
+import dev.racci.minix.api.plugin.logger.MinixLogger
 import org.koin.core.component.KoinComponent
+import org.koin.core.component.get
 import org.koin.core.component.inject
 
 interface UtilObject : KoinComponent {
 
     val minix: MinixPlugin
+
+    val logger: MinixLogger get() = get()
 
     companion object : UtilObject {
         override val minix: MinixPlugin by inject()

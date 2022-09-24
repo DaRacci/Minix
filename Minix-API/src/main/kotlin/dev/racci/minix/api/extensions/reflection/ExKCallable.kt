52c70b68629dabee6b21726b6c85904f39e98af0
DaRacci <racci@racci.dev>
2022-09-24 17:04:15 +1000

    fix(API): KCallable params

--- Minix-API/src/main/kotlin/dev/racci/minix/api/extensions/reflection/ExKCallable.kt
+++ Minix-API/src/main/kotlin/dev/racci/minix/api/extensions/reflection/ExKCallable.kt
@@ -1,9 +1,10 @@
 package dev.racci.minix.api.extensions.reflection
 
 import dev.racci.minix.api.utils.reflection.AccessUtils
+import org.checkerframework.checker.units.qual.A
 import kotlin.reflect.KCallable
 
 /** @see [AccessUtils.accessWith] */
-inline fun <R, A, C : KCallable<R>> C.accessWith(
+inline fun <R, C : KCallable<*>> C.accessWith(
     action: C.() -> A
 ): A = AccessUtils.accessWith(this, action)

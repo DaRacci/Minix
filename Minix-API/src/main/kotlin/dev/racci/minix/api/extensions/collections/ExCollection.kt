c7d0a5100698c8c32d3ed102358d6fa7600ea834
DaRacci <racci@racci.dev>
2022-10-04 22:20:02 +1100

    feat(Utils): Add overloads for KProperty types

--- Minix-API/src/main/kotlin/dev/racci/minix/api/extensions/collections/ExCollection.kt
+++ Minix-API/src/main/kotlin/dev/racci/minix/api/extensions/collections/ExCollection.kt
@@ -2,8 +2,15 @@ package dev.racci.minix.api.extensions.collections
 
 import arrow.core.Option
 import arrow.core.Validated
+import arrow.core.filterIsInstance
 import dev.racci.minix.api.utils.collections.CollectionUtils
 import kotlin.reflect.KCallable
+import kotlin.reflect.KMutableProperty0
+import kotlin.reflect.KMutableProperty1
+import kotlin.reflect.KMutableProperty2
+import kotlin.reflect.KProperty0
+import kotlin.reflect.KProperty1
+import kotlin.reflect.KProperty2
 
 /** @see CollectionUtils.If.ifEmpty */
 suspend inline fun <R, C : Collection<*>> C.ifEmpty(
@@ -31,6 +38,48 @@ inline fun <T : KCallable<*>> Collection<T>.findKCallable(
     name: String,
     ignoreCase: Boolean = false
 ): Option<T> = CollectionUtils.Find.findKCallable(this, name, ignoreCase)
+
+/** @see CollectionUtils.Find.findKCallable */
+@JvmName("findKProperty0")
+inline fun <reified R> Collection<KProperty0<*>>.findKProperty(
+    name: String,
+    ignoreCase: Boolean = false
+): Option<KProperty0<R>> = CollectionUtils.Find.findKCallable(this, name, ignoreCase).filterIsInstance()
+
+/** @see CollectionUtils.Find.findKCallable */
+@JvmName("findKProperty1")
+inline fun <reified R> Collection<KProperty1<*, *>>.findKProperty(
+    name: String,
+    ignoreCase: Boolean = false
+): Option<KProperty1<Any, R>> = CollectionUtils.Find.findKCallable(this, name, ignoreCase).filterIsInstance()
+
+/** @see CollectionUtils.Find.findKCallable */
+@JvmName("findKProperty2")
+inline fun <reified R> Collection<KProperty2<*, *, *>>.findKProperty(
+    name: String,
+    ignoreCase: Boolean = false
+): Option<KProperty2<*, *, R>> = CollectionUtils.Find.findKCallable(this, name, ignoreCase).filterIsInstance()
+
+/** @see CollectionUtils.Find.findKCallable */
+@JvmName("findKPropertyMutable0")
+inline fun <reified R> Collection<KMutableProperty0<*>>.findKProperty(
+    name: String,
+    ignoreCase: Boolean = false
+): Option<KMutableProperty0<R>> = CollectionUtils.Find.findKCallable(this, name, ignoreCase).filterIsInstance()
+
+/** @see CollectionUtils.Find.findKCallable */
+@JvmName("findKPropertyMutable1")
+inline fun <reified R> Collection<KMutableProperty1<*, *>>.findKProperty(
+    name: String,
+    ignoreCase: Boolean = false
+): Option<KMutableProperty1<*, R>> = CollectionUtils.Find.findKCallable(this, name, ignoreCase).filterIsInstance()
+
+/** @see CollectionUtils.Find.findKCallable */
+@JvmName("findKPropertyMutable2")
+inline fun <reified R> Collection<KMutableProperty2<*, *, *>>.findKProperty(
+    name: String,
+    ignoreCase: Boolean = false
+): Option<KMutableProperty2<*, *, R>> = CollectionUtils.Find.findKCallable(this, name, ignoreCase).filterIsInstance()
 
 /** @see CollectionUtils.Mutate.clear */
 suspend inline fun <V> MutableCollection<V>.clear(

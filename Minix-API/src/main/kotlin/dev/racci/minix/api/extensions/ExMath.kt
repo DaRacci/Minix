90c26a00356de212388b842531e38386f96d140e
DaRacci <racci@racci.dev>
2022-10-25 14:27:08 +1100

    chore: Remove leftover classes

--- Minix-API/src/main/kotlin/dev/racci/minix/api/extensions/ExMath.kt
+++
@@ -1,6 +0,0 @@
-package dev.racci.minix.api.extensions
-
-import java.math.BigDecimal
-import java.math.RoundingMode
-
-fun Double.round(decimals: Int) = BigDecimal(this).setScale(decimals, RoundingMode.HALF_EVEN).toDouble()

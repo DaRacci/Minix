423987b555680d053ca6856c725285e436465852
DaRacci <racci@racci.dev>
2022-10-25 14:34:48 +1100

    feat(Logger): LoggingFactory for (in the future) scoped sub loggers

---
+++ minix-plugin/api-common/src/main/kotlin/dev/racci/minix/api/logger/MinixLoggerFactory.kt
@@ -0,0 +1,24 @@
+package dev.racci.minix.api.logger
+
+import arrow.core.Option
+import arrow.core.getOrElse
+import dev.racci.minix.api.PlatformProxy
+import dev.racci.minix.api.plugin.MinixPlugin
+import dev.racci.minix.api.services.PluginService
+import org.apiguardian.api.API
+import org.koin.core.component.KoinComponent
+import org.koin.core.component.get
+import kotlin.reflect.KProperty
+
+// TODO -> Add support for scopes, which wrap the parent logger.
+@API(status = API.Status.EXPERIMENTAL, since = "5.0.0")
+public object MinixLoggerFactory : KoinComponent {
+    private val EXISTING: MutableMap<MinixPlugin, MinixLogger> = mutableMapOf()
+
+    public operator fun getValue(
+        thisRef: Any,
+        property: KProperty<*>
+    ): MinixLogger = Option.catch { PluginService.fromClassloader(thisRef::class.java.classLoader)!! }
+        .map { EXISTING.getOrPut(it) { get<PlatformProxy>().createLogger(it) } }
+        .getOrElse { get() }
+}

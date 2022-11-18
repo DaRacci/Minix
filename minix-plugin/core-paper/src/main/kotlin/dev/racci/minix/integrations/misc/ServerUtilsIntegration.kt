02ba36059cee56e1d230ff9171b464c8a1896f46
DaRacci <racci@racci.dev>
2022-11-18 13:20:48 +1100

    refactor(ServerUtils): Flow subscription

--- minix-plugin/core-paper/src/main/kotlin/dev/racci/minix/integrations/misc/ServerUtilsIntegration.kt
+++ minix-plugin/core-paper/src/main/kotlin/dev/racci/minix/integrations/misc/ServerUtilsIntegration.kt
@@ -1,32 +1,27 @@
 package dev.racci.minix.integrations.misc
 
-import dev.racci.minix.api.extensions.reflection.safeCast
-import dev.racci.minix.api.logger.MinixLoggerFactory
+import dev.racci.minix.api.data.Priority
 import dev.racci.minix.api.plugin.MinixPlugin
 import dev.racci.minix.api.services.PluginService
 import dev.racci.minix.api.utils.getKoin
 import dev.racci.minix.flowbus.receiver.EventReceiver
-import dev.racci.minix.flowbus.subscribe
+import dev.racci.minix.flowbus.subscribeFlow
 import dev.racci.minix.integrations.Integration
 import dev.racci.minix.integrations.annotations.MappedIntegration
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.flow.filter
+import kotlinx.coroutines.flow.mapNotNull
 import net.frankheijden.serverutils.bukkit.events.BukkitPluginUnloadEvent
 import net.frankheijden.serverutils.common.events.PluginEvent
 
 @MappedIntegration("ServerUtils")
 public abstract class ServerUtilsIntegration : Integration, EventReceiver by getKoin().get() {
-    private val logger by MinixLoggerFactory
-
     public override suspend fun handleEnable() {
-        subscribe<BukkitPluginUnloadEvent> {
-            if (stage == PluginEvent.Stage.POST) {
-                logger.debug { "Ignoring post disable event for ${plugin.name}" }
-                return@subscribe
-            }
-
-            logger.debug { "Handling pre unload event for ${plugin.name}" }
-
-            val plugin = plugin.safeCast<MinixPlugin>() ?: return@subscribe
-            PluginService.unloadPlugin(plugin)
+        coroutineScope {
+            subscribeFlow<BukkitPluginUnloadEvent>(Priority.MONITOR)
+                .filter { event -> event.stage == PluginEvent.Stage.PRE }
+                .mapNotNull { event -> event.plugin as? MinixPlugin }
+                .collect(PluginService::unloadPlugin)
         }
     }
 }

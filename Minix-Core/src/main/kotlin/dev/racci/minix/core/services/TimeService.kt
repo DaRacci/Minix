3931e462687b15654d36cb016de00bd7a2a8f528
DaRacci <racci@racci.dev>
2022-11-28 17:34:56 +1100

    fix(TimeService): Fire event with no players online.
    Prevents some weird edge cases and confusion to why a become day event would fire at eg: 4pm when the first player joins

--- Minix-Core/src/main/kotlin/dev/racci/minix/core/services/TimeService.kt
+++ Minix-Core/src/main/kotlin/dev/racci/minix/core/services/TimeService.kt
@@ -1,41 +1,36 @@
 package dev.racci.minix.core.services
 
+import arrow.fx.coroutines.Atomic
+import com.github.benmanes.caffeine.cache.Caffeine
 import dev.racci.minix.api.annotations.MappedExtension
 import dev.racci.minix.api.coroutine.launch
-import dev.racci.minix.api.events.WorldDayEvent
-import dev.racci.minix.api.events.WorldNightEvent
+import dev.racci.minix.api.events.world.WorldDayEvent
+import dev.racci.minix.api.events.world.WorldNightEvent
 import dev.racci.minix.api.extension.Extension
 import dev.racci.minix.api.extensions.event
-import dev.racci.minix.api.extensions.onlinePlayers
 import dev.racci.minix.api.extensions.scheduler
 import dev.racci.minix.api.extensions.ticks
-import dev.racci.minix.api.extensions.world
 import dev.racci.minix.api.extensions.worlds
 import dev.racci.minix.api.plugin.Minix
-import dev.racci.minix.api.utils.collections.CollectionUtils.cacheOf
-import kotlinx.atomicfu.AtomicBoolean
-import kotlinx.atomicfu.atomic
 import org.bukkit.World
 import org.bukkit.event.world.WorldLoadEvent
-import org.bukkit.event.world.WorldUnloadEvent
 import kotlin.reflect.full.primaryConstructor
 import kotlin.time.Duration.Companion.milliseconds
 
 @MappedExtension(Minix::class, "Time Service")
 class TimeService(override val plugin: Minix) : Extension<Minix>() {
-
-    private val timeState = cacheOf<String, AtomicBoolean> { atomic(world(this)!!.isDayTime) }
+    private val timeStates = Caffeine.newBuilder().weakKeys()
+        .build<World, Atomic<Boolean>> { Atomic.unsafe(it.isDayTime) }
 
     override suspend fun handleEnable() {
         event<WorldLoadEvent> { checkTime(world) }
-        event<WorldUnloadEvent> { timeState.invalidate(world.name) }
 
-        scheduler { if (onlinePlayers.isNotEmpty()) worlds.forEach(::checkTime) }.runAsyncTaskTimer(plugin, 15.milliseconds, 10.ticks)
+        scheduler { worlds.forEach { world -> checkTime(world) } }.runAsyncTaskTimer(plugin, 15.milliseconds, 10.ticks)
     }
 
-    private fun checkTime(world: World) {
+    private suspend fun checkTime(world: World) {
         val isDay = world.isDayTime
-        val wasDay = timeState[world.name].getAndSet(isDay)
+        val wasDay = timeStates[world].getAndSet(isDay)
 
         if (isDay == wasDay) return
 
